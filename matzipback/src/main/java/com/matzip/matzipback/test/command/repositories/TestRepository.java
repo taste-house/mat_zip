9e73ff94ffb5ffb3c515be5c429bbbc194c7d144
leebackcoding <lcy2825@gmail.com>
2024-09-19 17:52:24 +0900

    feat : TestRepository 추가

--- matzipback/src/main/java/com/matzip/matzipback/test/command/repositories/TestRepository.java
+++ matzipback/src/main/java/com/matzip/matzipback/test/command/repositories/TestRepository.java
@@ -1,6 +1,6 @@
 package com.matzip.matzipback.test.command.repositories;
 
-import com.mamully.toyProject.test.command.entities.Test;
+import com.matzip.matzipback.test.command.entities.Test;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TestRepository extends JpaRepository<Test, Long> {
