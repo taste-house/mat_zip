5263453413242f8c3b09357bbb5fb57345362430
leebackcoding <lcy2825@gmail.com>
2024-09-20 14:10:13 +0900

    chore: 테스트 코드 제거

--- matzipback/src/main/java/com/matzip/matzipback/test/command/repositories/TestRepository.java
+++
@@ -1,7 +0,0 @@
-package com.matzip.matzipback.test.command.repositories;
-
-import com.matzip.matzipback.test.command.entities.Test;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TestRepository extends JpaRepository<Test, Long> {
-}
