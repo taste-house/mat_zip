0e4ecf42fc72839984dc22c5c5e4c8648c9771ba
JIYOUNG-22 <dyong0922@naver.com>
2024-09-21 11:33:36 +0900

    feat: PostImage Entity 추가

---
+++ matzipback/src/main/java/com/matzip/matzipback/test/command/domain/aggregate/PostImage.java
@@ -0,0 +1,25 @@
+package com.matzip.matzipback.test.command.domain.aggregate;
+
+import jakarta.persistence.*;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "post_image")
+@NoArgsConstructor
+@SQLDelete(sql = "UPDATE post_image SET post_image_deleted_yn = 'Y' WHERE post_image_seq = ?")
+public class PostImage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int postImageSeq;
+    private int postSeq;
+    private String postImageFilepath;
+    private String postImageName;
+    private String postImageUrl;
+    private LocalDateTime postImageCreatedTime;
+    private String postImageDeletedYn;
+
+}
