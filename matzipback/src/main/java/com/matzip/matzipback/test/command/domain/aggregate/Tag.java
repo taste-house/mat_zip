2de1491a08cb44a5020cc49352b3775847d83440
JIYOUNG-22 <dyong0922@naver.com>
2024-09-21 11:33:51 +0900

    feat: Tag Entity 추가

---
+++ matzipback/src/main/java/com/matzip/matzipback/test/command/domain/aggregate/Tag.java
@@ -0,0 +1,16 @@
+package com.matzip.matzipback.test.command.domain.aggregate;
+
+import jakarta.persistence.*;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tag")
+@NoArgsConstructor
+public class Tag {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int tagSeq;
+    private String tagName;
+
+}
