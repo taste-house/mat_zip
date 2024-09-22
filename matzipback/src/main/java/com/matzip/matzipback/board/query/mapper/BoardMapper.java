a5b8e8977ba2411c1fee5b0f018d20e70827706d
leebackcoding <lcy2825@gmail.com>
2024-09-22 22:22:22 +0900

    feat: 게시판 카테고리 조회 mapper method 추가

--- matzipback/src/main/java/com/matzip/matzipback/board/query/mapper/BoardMapper.java
+++ matzipback/src/main/java/com/matzip/matzipback/board/query/mapper/BoardMapper.java
@@ -1,4 +1,12 @@
 package com.matzip.matzipback.board.query.mapper;
 
+import com.matzip.matzipback.board.query.dto.BoardCategoryDTO;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
 public interface BoardMapper {
+
+    List<BoardCategoryDTO> getBoardCategories();
 }
