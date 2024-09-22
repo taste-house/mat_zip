61510f474c9666ea7eb4c10ff194d6cc07f715fa
leebackcoding <lcy2825@gmail.com>
2024-09-22 22:18:22 +0900

    feat: 게시판 카테고리 조회 DTO 추가

---
+++ matzipback/src/main/java/com/matzip/matzipback/board/query/dto/BoardCategoryDTO.java
@@ -0,0 +1,15 @@
+package com.matzip.matzipback.board.query.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@AllArgsConstructor
+public class BoardCategoryDTO {
+
+    private String BoardCategoryName;
+}
