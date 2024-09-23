f2ddb4e6cbd0afc42d238a5bf259051de0180875
wildcat222 <wildcat222@naver.com>
2024-09-23 18:47:13 +0900

    feat:게시글 댓글 작성 controller 추가

--- matzipback/src/main/java/com/matzip/matzipback/board/command/application/controller/PostCommentController.java
+++ matzipback/src/main/java/com/matzip/matzipback/board/command/application/controller/PostCommentController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/")
+@RequestMapping("/api/v1")
 public class PostCommentController {
 
     private final PostCommentService postCommentService;
