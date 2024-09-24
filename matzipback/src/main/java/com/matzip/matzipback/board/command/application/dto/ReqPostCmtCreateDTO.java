dbef5736ecb559f92dbe0756cf2a3daf2ab6e9f6
wildcat222 <wildcat222@naver.com>
2024-09-24 15:00:08 +0900

    chore: 댓글 작성 요청, 응답 DTO 명 수정

--- matzipback/src/main/java/com/matzip/matzipback/board/command/application/dto/ReqPostCmtCreateDTO.java
+++ matzipback/src/main/java/com/matzip/matzipback/board/command/application/dto/ReqPostCmtCreateDTO.java
@@ -2,13 +2,10 @@ package com.matzip.matzipback.board.command.application.dto;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.data.annotation.CreatedDate;
-
-import java.time.LocalDateTime;
 
 @Getter
 @Setter
-public class RequestPostCommentDTO {
+public class ReqPostCmtCreateDTO {
 
     private Long postSeq;
     private String postCommentContent;
--- matzipback/src/main/java/com/matzip/matzipback/board/command/application/dto/ResPostCmtCreateDTO.java
+++ matzipback/src/main/java/com/matzip/matzipback/board/command/application/dto/ResPostCmtCreateDTO.java
@@ -5,7 +5,7 @@ import lombok.Setter;
 
 @Getter
 @Setter
-public class ResponsePostCommentDTO {
+public class ResPostCmtCreateDTO {
 
     private Long postSeq;
 }
