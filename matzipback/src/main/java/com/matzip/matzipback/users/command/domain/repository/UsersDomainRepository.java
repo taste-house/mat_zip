6ffc6ec81148923d13e5c60c3613693dccd10493
leebackcoding <lcy2825@gmail.com>
2024-09-24 17:41:40 +0900

    feat: 회원가입, 이메일로 유저 찾기 메소드 추가

--- matzipback/src/main/java/com/matzip/matzipback/users/command/domain/repository/UsersDomainRepository.java
+++ matzipback/src/main/java/com/matzip/matzipback/users/command/domain/repository/UsersDomainRepository.java
@@ -1,4 +1,12 @@
 package com.matzip.matzipback.users.command.domain.repository;
 
-public class UsersDomainRepository {
+import com.matzip.matzipback.users.command.domain.aggregate.Users;
+
+import java.util.Optional;
+
+public interface UsersDomainRepository {
+
+    Users save(Users users);
+
+    Optional<Users> findByUserEmail(String userEmail);
 }
