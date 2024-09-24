2e6862ff93ad93ca6a2ec310950eeec7025917b2
wildcat222 <wildcat222@naver.com>
2024-09-24 17:14:51 +0900

    chore: 응답 DTO 통합

--- matzipback/src/main/java/com/matzip/matzipback/board/command/application/dto/ResPostCmtDTO.java
+++ matzipback/src/main/java/com/matzip/matzipback/board/command/application/dto/ResPostCmtDTO.java
@@ -1,10 +1,14 @@
 package com.matzip.matzipback.board.command.application.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class ResPostCmtDTO {
 
     private Long postSeq;
