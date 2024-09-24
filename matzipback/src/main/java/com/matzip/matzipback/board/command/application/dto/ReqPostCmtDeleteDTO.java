309dfc885751fcd0696759a0f3977393c67c32ff
wildcat222 <wildcat222@naver.com>
2024-09-24 18:55:00 +0900

    feat: ReqPostCmtDeleteDTO 생성

---
+++ matzipback/src/main/java/com/matzip/matzipback/board/command/application/dto/ReqPostCmtDeleteDTO.java
@@ -0,0 +1,12 @@
+package com.matzip.matzipback.board.command.application.dto;
+
+import jdk.jfr.StackTrace;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ReqPostCmtDeleteDTO {
+
+    private Long postCommentSeq;
+}
