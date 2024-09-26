870705fcbe85fc3c9f857b89768f0a7fdf498073
김윤후 <gim-yunhu@gim-yunhuui-MacBookAir.local>
2024-09-26 12:47:40 +0900

    fix: #20 리스트 서랍 조회 기능 어노테이션 수정

--- matzipback/src/main/java/com/matzip/matzipback/matzipList/query/controller/ListQueryController.java
+++ matzipback/src/main/java/com/matzip/matzipback/matzipList/query/controller/ListQueryController.java
@@ -19,8 +19,9 @@ public class ListQueryController {
     private final ListQueryService listQueryService;
 
     // 유저 본인의 리스트 서랍 조회(모든 리스트 상태 조회)
-    @GetMapping("/listBox/{listUserSeq}")
+    @GetMapping("/listBox")
     public ResponseEntity<List<ListSearchAllDTO>> getListBox() {
+        //URL에서 유저 시퀀스 받지 않고 로그인한 사용자의 유저 시퀀스를 받아오는 것으로 구현
         Long listUserSeq = CustomUserUtils.getCurrentUserSeq();
         return ResponseEntity.ok().body(listQueryService.getListBox(listUserSeq));
     }
