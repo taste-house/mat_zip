8856ba3e94e25cf14f826ee7e84c344d9ae6c9b6
김윤후 <gim-yunhu@gim-yunhuui-MacBookAir.local>
2024-09-26 11:51:25 +0900

    feat: #20 리스트 서랍 조회 기능 구현

--- matzipback/src/main/java/com/matzip/matzipback/matzipList/query/controller/ListQueryController.java
+++ matzipback/src/main/java/com/matzip/matzipback/matzipList/query/controller/ListQueryController.java
@@ -1,4 +1,30 @@
 package com.matzip.matzipback.matzipList.query.controller;
 
+import com.matzip.matzipback.board.query.controller.BoardQueryController;
+import com.matzip.matzipback.common.util.CustomUserUtils;
+import com.matzip.matzipback.matzipList.query.dto.ListCategoryDTO;
+import com.matzip.matzipback.matzipList.query.dto.ListSearchAllDTO;
+import com.matzip.matzipback.matzipList.query.service.ListQueryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1")
 public class ListQueryController {
+
+    private final ListQueryService listQueryService;
+    private final BoardQueryController boardQueryController;
+
+    @GetMapping("/listBox/{listUserSeq}")
+    public ResponseEntity<List<ListSearchAllDTO>> getListBox() {
+        Long listUserSeq = CustomUserUtils.getCurrentUserSeq();
+        return ResponseEntity.ok().body(listQueryService.getListBox(listUserSeq));
+    }
+
+
+
 }
--- matzipback/src/main/java/com/matzip/matzipback/matzipList/query/mapper/ListQueryMapper.java
+++ matzipback/src/main/java/com/matzip/matzipback/matzipList/query/mapper/ListQueryMapper.java
@@ -1,4 +1,20 @@
 package com.matzip.matzipback.matzipList.query.mapper;
 
+import com.matzip.matzipback.matzipList.query.dto.ListCategoryDTO;
+import com.matzip.matzipback.matzipList.query.dto.ListSearchAllDTO;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
 public interface ListQueryMapper {
+
+    List<ListSearchAllDTO> getListBox(long listUserSeq);
+
+
+    
+    Long getCountList(long listUserSeq); // 각 사용자의 리스트 개수 구하기
+
+
+
 }
