da7dfb29746b690385ec4f70a9c57eaf7cb5533a
leebackcoding <lcy2825@gmail.com>
2024-09-26 17:06:39 +0900

    feat: like를 만드는 create 메서드 추가

--- matzipback/src/main/java/com/matzip/matzipback/like/command/domain/aggregate/Like.java
+++ matzipback/src/main/java/com/matzip/matzipback/like/command/domain/aggregate/Like.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "like")
+@Table(name = "`like`")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Like {
