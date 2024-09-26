957b6bba3cc71573f7dd0a1bdb70398e8200b72a
catnine11 <catnine11@gmail.com>
2024-09-26 21:12:20 +0900

    chore: 유효성 검사 및 로그 추가

--- matzipback/src/main/java/com/matzip/matzipback/users/command/application/controller/UsersCommandController.java
+++ matzipback/src/main/java/com/matzip/matzipback/users/command/application/controller/UsersCommandController.java
@@ -2,22 +2,32 @@ package com.matzip.matzipback.users.command.application.controller;
 
 import com.matzip.matzipback.users.command.application.service.UsersCommandService;
 import com.matzip.matzipback.users.command.dto.CreateUserRequest;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1")
-public class UsersCommandContoller {
+@Slf4j
+public class UsersCommandController {
 
     private final UsersCommandService UsersCommandService;
 
     /* 회원가입 기능*/
     @PostMapping("/auth/register")
-    public ResponseEntity<Void> createUser(@RequestBody CreateUserRequest newUser) {
-
+    public ResponseEntity<String> createUser(@Valid @RequestBody CreateUserRequest newUser, BindingResult bindingResult) {
+        // 유효성 검사 오류가 있는 경우 처리
+        if (bindingResult.hasErrors()) {
+            // 첫 번째 오류 메시지를 반환
+            String errorMessage = bindingResult.getAllErrors().get(0).getDefaultMessage();
+            return ResponseEntity.badRequest().body(errorMessage);
+        }
+        log.info("회원가입 요청 createUser: {}", newUser);
         UsersCommandService.createUser(newUser);
 
         return ResponseEntity.status(HttpStatus.CREATED).build();
