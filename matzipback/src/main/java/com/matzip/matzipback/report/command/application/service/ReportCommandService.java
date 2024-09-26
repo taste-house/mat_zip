9fba191149e880f340000497598b1050ce00c948
leebackcoding <lcy2825@gmail.com>
2024-09-26 12:44:29 +0900

    feat: 게시글 신고 Service 추가

--- matzipback/src/main/java/com/matzip/matzipback/report/command/application/service/ReportCommandService.java
+++ matzipback/src/main/java/com/matzip/matzipback/report/command/application/service/ReportCommandService.java
@@ -1,4 +1,37 @@
 package com.matzip.matzipback.report.command.application.service;
 
+import com.matzip.matzipback.board.command.domain.aggregate.Post;
+import com.matzip.matzipback.board.command.domain.service.PostDomainService;
+import com.matzip.matzipback.report.command.domain.aggregate.Report;
+import com.matzip.matzipback.report.command.domain.repository.ReportDomainRepository;
+import com.matzip.matzipback.report.command.domain.service.ReportDomainService;
+import com.matzip.matzipback.report.command.dto.PostReportReqDTO;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class ReportCommandService {
+
+    private final PostDomainService postDomainService;
+    private final ReportDomainService reportDomainService;
+
+    // 게시글 신고 기능
+    public Report savePostReport(PostReportReqDTO postReportReqDTO) {
+
+        Post foundPost = postDomainService.findByPostSeq(postReportReqDTO.getPostSeq());
+        Long reportedUserSeq = foundPost.getPostUserSeq();
+        Long reporterUserSeq = /*CustomUserUtils.getCurrentUserSeq();*/ 2L;
+
+        boolean isExistReport = reportDomainService.checkReportExists(reporterUserSeq, postReportReqDTO.getPostSeq());
+
+        if (isExistReport) {
+            return null;
+        }
+
+        Report newReport = Report.getReportSeq(reporterUserSeq, reportedUserSeq, postReportReqDTO);
+
+        return reportDomainService.saveReport(newReport);
+    }
 }
