b2fe9fc647bfa08048943244d7c9b38177280f57
leebackcoding <lcy2825@gmail.com>
2024-09-26 17:19:01 +0900

    feat: 회원가입 후 바로 UserActivity 만드는 로직 추가

--- matzipback/src/main/java/com/matzip/matzipback/users/command/application/service/UsersCommandService.java
+++ matzipback/src/main/java/com/matzip/matzipback/users/command/application/service/UsersCommandService.java
@@ -1,7 +1,9 @@
 package com.matzip.matzipback.users.command.application.service;
 
+import com.matzip.matzipback.users.command.domain.aggregate.UserActivity;
 import com.matzip.matzipback.users.command.domain.aggregate.Users;
 import com.matzip.matzipback.users.command.domain.repository.UsersDomainRepository;
+import com.matzip.matzipback.users.command.domain.service.UserActivityDomainService;
 import com.matzip.matzipback.users.command.dto.CreateUserRequest;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
@@ -15,10 +17,13 @@ public class UsersCommandService {
     private final UsersDomainRepository usersDomainRepository;
     private final ModelMapper modelMapper;
     private final BCryptPasswordEncoder passwordEncoder;
+    private final UserActivityDomainService userActivityDomainService;
 
     public void createUser(CreateUserRequest newUser) {
         Users users = modelMapper.map(newUser, Users.class);
         users.encryptPassword(passwordEncoder.encode(newUser.getUserPassword()));
         usersDomainRepository.save(users);
+
+        userActivityDomainService.saveUserActivity(UserActivity.create(users.getUserSeq()));
     }
 }
