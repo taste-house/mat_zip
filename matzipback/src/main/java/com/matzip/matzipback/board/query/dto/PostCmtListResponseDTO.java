e07fb2333c1a42dd677bd04b7cacf503b70db017
wildcat222 <wildcat222@naver.com>
2024-09-26 21:47:53 +0900

    chore: 변수명 수정 및 설명 주석 수정

--- matzipback/src/main/java/com/matzip/matzipback/board/query/dto/PostCmtListResponseDTO.java
+++ matzipback/src/main/java/com/matzip/matzipback/board/query/dto/PostCmtListResponseDTO.java
@@ -8,8 +8,8 @@ import java.util.List;
 @Getter
 @Builder
 public class PostCmtListResponseDTO {
-    private List<PostCommentDTO> comments;    // 검색조건에 부합하는 게시글 리스트
+    private List<PostCommentDTO> comments;    // 회원 본인이 작성한 댓글 리스트
     private int currentPage;        // 현재 페이지
     private int totalPages;         // 전체 페이지 수
-    private long totalPosts;        // 검색조건에 부합하는 총 게시글 수
+    private long totalPostComments;        // 회원 본인이 작성한 총 댓글 수
 }
