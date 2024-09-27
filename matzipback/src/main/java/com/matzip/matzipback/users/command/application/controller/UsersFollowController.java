f3170d2ffcb47adc4f1175d847f38d08aa6e205f
leebackcoding <lcy2825@gmail.com>
2024-09-28 02:32:57 +0900

    feat: 팔로우 요청에 대한 Controller 추가

---
+++ matzipback/src/main/java/com/matzip/matzipback/users/command/application/controller/UsersFollowController.java
@@ -0,0 +1,37 @@
+package com.matzip.matzipback.users.command.application.controller;
+
+import com.matzip.matzipback.responsemessage.ResponseMessage;
+import com.matzip.matzipback.users.command.application.service.UsersFollowService;
+import com.matzip.matzipback.users.command.dto.FollowResMessageDTO;
+import com.matzip.matzipback.users.command.dto.FollowDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/follow")
+public class UsersFollowController {
+
+    private final UsersFollowService usersFollowService;
+    
+    // 팔로우 버튼을 누르는 기능
+    // 팔로우가 되어 있으면 팔로우 취소됨
+    @PostMapping
+    public ResponseEntity<FollowResMessageDTO> doFollow(@RequestBody FollowDTO followDTO) {
+        boolean result = usersFollowService.doFollow(followDTO);
+
+        // 팔로우 성공
+        if (result) {
+            return ResponseEntity.ok(new FollowResMessageDTO(HttpStatus.OK.value(), ResponseMessage.FOLLOW_SUCCESS.getMessage()));
+        }
+
+        // 팔로우 취소
+        return ResponseEntity.ok(new FollowResMessageDTO(HttpStatus.OK.value(), ResponseMessage.FOLLOW_DELETE.getMessage()));
+    }
+
+}
