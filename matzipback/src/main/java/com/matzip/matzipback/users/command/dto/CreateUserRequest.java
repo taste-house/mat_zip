131dbd68a4b9f65f978da00a3c27d44d62a4668f
catnine11 <catnine11@gmail.com>
2024-09-27 20:52:02 +0900

    chore: 비밀번호 정규화패턴 특수문자 및 주석 추가

--- matzipback/src/main/java/com/matzip/matzipback/users/command/dto/CreateUserRequest.java
+++ matzipback/src/main/java/com/matzip/matzipback/users/command/dto/CreateUserRequest.java
@@ -16,8 +16,9 @@ public class CreateUserRequest {
     @Email(message = "유효한 이메일 주소를 입력해주세요.")
     private String userEmail;
     @NotBlank(message = "비밀번호는 필수 입력 사항입니다.")
-    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
+    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[!@#$%^&*.,?])[A-Za-z\\d!@#$%^&*.,?]{8,}$",
             message = "비밀번호는 영문, 숫자, 특수문자를 포함한 8자 이상이어야 합니다.")
+    // 특수문자는 !@#$%^&*.,? 만 허용
     private String userPassword;
     @NotBlank(message = "이름은 필수 입력 사항입니다.")
     private String userName;
