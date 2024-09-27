19d48bdb4d0a40e73c6c8a6203f350032ef2451b
leebackcoding <lcy2825@gmail.com>
2024-09-28 04:07:29 +0900

    feat: 팔로잉 조회를 위한 DTO 추가

---
+++ matzipback/src/main/java/com/matzip/matzipback/users/query/dto/FollowingUsersDTO.java
@@ -0,0 +1,11 @@
+package com.matzip.matzipback.users.query.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class FollowingUsersDTO {
+
+    private String userNickName;
+}
