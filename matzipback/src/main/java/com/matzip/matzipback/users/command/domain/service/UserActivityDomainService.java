4db5e5a661b5281c8cd558a0cb7dcd6eb4213007
leebackcoding <lcy2825@gmail.com>
2024-09-28 22:24:13 +0900

    feat: 회원 활동 포인트강 업데이트 할 때 등급도 변경되도록 구현

--- matzipback/src/main/java/com/matzip/matzipback/users/command/domain/service/UserActivityDomainService.java
+++ matzipback/src/main/java/com/matzip/matzipback/users/command/domain/service/UserActivityDomainService.java
@@ -1,16 +1,20 @@
 package com.matzip.matzipback.users.command.domain.service;
 
+import com.matzip.matzipback.users.command.domain.aggregate.ActiveLevel;
 import com.matzip.matzipback.users.command.domain.aggregate.UserActivity;
 import com.matzip.matzipback.users.command.domain.repository.UserActivityRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class UserActivityDomainService {
 
     private final UserActivityRepository userActivityRepository;
+    private final UserActiveLevelDomainService userActiveLevelDomainService;
 
     // 유저 활동 포인트를 업데이트 하는 메서드
     @Transactional
@@ -21,12 +25,37 @@ public class UserActivityDomainService {
 
         UserActivity foundUserActivity = userActivityRepository.findById(activityUserSeq).orElse(null);
 
+        if (foundUserActivity == null) {
+            UserActivity createdUserActivity = UserActivity.create(activityUserSeq);
+            foundUserActivity = saveUserActivity(createdUserActivity);
+        }
         foundUserActivity.changePoint(point);
+
+        updateUserActivityLevel(foundUserActivity);
     }
 
     // 유저 활동 정보 만들기
-    public void saveUserActivity(UserActivity userActivity) {
-        userActivityRepository.save(userActivity);
+    public UserActivity saveUserActivity(UserActivity userActivity) {
+        return userActivityRepository.save(userActivity);
     }
 
+
+    // 유저 회원 등급 조정
+    public void updateUserActivityLevel(UserActivity userActivity) {
+
+        long activeLevelSeq = calculateLevel(userActivity.getActivityPoint());
+
+        userActivity.changeLevel(activeLevelSeq);
+    }
+
+    // 유저 회원 등급 조정
+    private long calculateLevel(int point) {
+        List<ActiveLevel> activeLevelList = userActiveLevelDomainService.getAllActiveLevel();
+        for (ActiveLevel level : activeLevelList) {
+            if (point >= level.getActiveLevelStandard()) {
+                return level.getActiveLevelSeq();
+            }
+        }
+        return 1;
+    }
 }
