5495150773acf0233e37db53b675d7d8d83d7127
leebackcoding <lcy2825@gmail.com>
2024-09-28 23:24:17 +0900

    feat: ActiveLevelDTO 추가

---
+++ matzipback/src/main/java/com/matzip/matzipback/users/query/dto/ActiveLevelDTO.java
@@ -0,0 +1,13 @@
+package com.matzip.matzipback.users.query.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ActiveLevelDTO {
+
+    private long activeLevelSeq;
+    private String activeLevelName;
+    private int activeLevelStandard;
+}
