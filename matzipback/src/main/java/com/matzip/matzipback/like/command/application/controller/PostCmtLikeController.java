b71eb72a3babfe4b4472ac1e50bb6d9b2f01981e
wildcat222 <wildcat222@naver.com>
2024-09-29 17:33:39 +0900

    refactor: 게시글 댓글 좋아요 메서드명 및 요청 값 처리과정 등 수정 및 성공 메세지 추가

--- matzipback/src/main/java/com/matzip/matzipback/like/command/application/controller/PostCmtLikeController.java
+++ matzipback/src/main/java/com/matzip/matzipback/like/command/application/controller/PostCmtLikeController.java
@@ -1,9 +1,17 @@
 package com.matzip.matzipback.like.command.application.controller;
 
+import com.matzip.matzipback.like.command.application.dto.PostCmtLikeReqDTO;
+import com.matzip.matzipback.like.command.application.dto.PostCmtLikeResMessageDTO;
 import com.matzip.matzipback.like.command.application.service.PostCmtLikeService;
+import com.matzip.matzipback.like.command.domain.aggregate.Like;
+import com.matzip.matzipback.responsemessage.ResponseMessage;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
@@ -14,13 +22,16 @@ public class PostCmtLikeController {
 
     // 게시글 댓글 좋아요
     @PostMapping("/{postSeq}/postcomment/{postCommentSeq}/like")
-    public ResponseEntity<Long> toggleLike(
-            @PathVariable Long postSeq,
-            @PathVariable Long postCommentSeq
-    ) {
-        postCmtLikeService.active(postCommentSeq);
+    public ResponseEntity<PostCmtLikeResMessageDTO> savePostCmtLike(@RequestBody PostCmtLikeReqDTO postCmtLikeReqDTO) {
+         Like resultLike = postCmtLikeService.savePostCmtLike(postCmtLikeReqDTO);
 
-        return ResponseEntity.ok(postSeq);
+        if (resultLike != null) {
+            // 좋아요 등록
+            return ResponseEntity.status(HttpStatus.CREATED).body(new PostCmtLikeResMessageDTO(201, ResponseMessage.SAVE_SUCCESS.getMessage(), resultLike.getPostSeq()));
+        }
+
+        // 좋아요 취소
+        return ResponseEntity.status(HttpStatus.OK).body(new PostCmtLikeResMessageDTO(200, ResponseMessage.SAVE_FAIL.getMessage(), postCmtLikeReqDTO.getPostSeq()));
     }
 
 }
