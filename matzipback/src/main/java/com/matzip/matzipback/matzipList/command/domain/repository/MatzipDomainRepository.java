db9b13b6a3923ca092a848f3c994b554914d923b
gim-yunhu <rlarjswn0303@gmail.com>
2024-09-29 17:46:31 +0900

    feat: #28 리스트 맛집 MatzipDomainRepository 수정

--- matzipback/src/main/java/com/matzip/matzipback/matzipList/command/domain/repository/MatzipDomainRepository.java
+++ matzipback/src/main/java/com/matzip/matzipback/matzipList/command/domain/repository/MatzipDomainRepository.java
@@ -1,7 +1,10 @@
 package com.matzip.matzipback.matzipList.command.domain.repository;
 
 import com.matzip.matzipback.matzipList.command.domain.aggregate.MyListMatzip;
+import jakarta.validation.constraints.NotBlank;
 
 public interface MatzipDomainRepository {
     MyListMatzip save(MyListMatzip newMatzip);
+
+    void deleteById(@NotBlank Long listMatzipSeq);
 }
