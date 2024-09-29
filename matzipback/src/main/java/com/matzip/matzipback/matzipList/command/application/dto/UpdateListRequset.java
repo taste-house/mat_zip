80f5da80829bc99e7db4305a8eb5dae90ec54f5f
gim-yunhu <rlarjswn0303@gmail.com>
2024-09-29 16:44:49 +0900

    feat: #25 리스트 수정 Update Repo추가

---
+++ matzipback/src/main/java/com/matzip/matzipback/matzipList/command/application/dto/UpdateListRequset.java
@@ -0,0 +1,16 @@
+package com.matzip.matzipback.matzipList.command.application.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+public class UpdateListRequset {
+    private Long listSeq;
+    private Long listUserSeq;
+    private String listTitle;
+    private String listContent;
+    private Integer listLevel;
+}
