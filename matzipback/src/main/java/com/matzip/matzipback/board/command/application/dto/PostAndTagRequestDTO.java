817022a709d741680206baf1722be84f4d8af3de
JIYOUNG-22 <dyong0922@naver.com>
2024-09-29 18:21:42 +0900

    chore: DTO 이름 수정

--- matzipback/src/main/java/com/matzip/matzipback/board/command/application/dto/PostAndTagRequestDTO.java
+++ matzipback/src/main/java/com/matzip/matzipback/board/command/application/dto/PostAndTagRequestDTO.java
@@ -7,12 +7,12 @@ import java.util.List;
 
 @Getter
 @Setter
-public class CreatePostAndTagReqDTO {
+public class PostAndTagRequestDTO {
     private String postTitle;       // 게시글 제목
     private Long postUserSeq;       // 게시물 작성자
     private String postContent;     // 게시글 본문
     private Long boardCategorySeq;  // 게시판 카테고리 고유번호
     private Long listSeq;           // 연결된 리스트 고유번호
     private Long restaurantSeq;     // 연결된 음식점 고유번호
-    private List<Long> tagSeq;    // 게시글 태그
+    private List<Long> tagSeq;      // 게시글 태그
 }
