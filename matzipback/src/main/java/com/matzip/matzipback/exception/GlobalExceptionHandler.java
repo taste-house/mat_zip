846017aea3c4973dec1800d6598354822e0ac2d0
catnine11 <catnine11@gmail.com>
2024-09-29 14:38:54 +0900

    chore: 유효값 검증 예외처리 전역적으로 변경을 위한 추가

--- matzipback/src/main/java/com/matzip/matzipback/exception/GlobalExceptionHandler.java
+++ matzipback/src/main/java/com/matzip/matzipback/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.matzip.matzipback.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -13,6 +14,14 @@ public class GlobalExceptionHandler {
     public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException e) {
         // 예외 메시지를 클라이언트에 반환
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+
+    // MethodArgumentNotValidException 발생 시 메시지 반환
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        // 첫 번째 오류 메시지를 추출하여 응답
+        String errorMessage = ex.getBindingResult().getAllErrors().get(0).getDefaultMessage();
+        return ResponseEntity.badRequest().body(errorMessage);
     }
 
 
