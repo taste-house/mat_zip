6afdfc84efdc788a18bd0ade922b1cd6f6619fc8
JIYOUNG-22 <dyong0922@naver.com>
2024-09-29 18:19:23 +0900

    feat: 게시글 수정을 위한 PostCommandService 추가

--- matzipback/src/main/java/com/matzip/matzipback/board/command/application/service/PostCommandService.java
+++ matzipback/src/main/java/com/matzip/matzipback/board/command/application/service/PostCommandService.java
@@ -1,7 +1,7 @@
 package com.matzip.matzipback.board.command.application.service;
 
 
-import com.matzip.matzipback.board.command.application.dto.CreatePostAndTagReqDTO;
+import com.matzip.matzipback.board.command.application.dto.PostAndTagRequestDTO;
 import com.matzip.matzipback.board.command.domain.aggregate.Post;
 import com.matzip.matzipback.board.command.domain.aggregate.PostTag;
 import com.matzip.matzipback.board.command.domain.repository.PostRepository;
@@ -10,6 +10,7 @@ import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 
 @Service
@@ -20,8 +21,9 @@ public class PostCommandService {
     private final PostTagRepository postTagRepository;
     private final ModelMapper modelMapper;
 
-    /* 1. 게시글 등록 */
-    public Long createPost(CreatePostAndTagReqDTO newPost) {
+    /* 1. 게시글, 태그 등록 */
+    @Transactional
+    public Long createPost(PostAndTagRequestDTO newPost) {
 
         // 나중에 Authorization 에서 빼와야한다. JwtUtil 에서의 메서드 활용할 것임
         Long userSeq = 1L;
@@ -45,7 +47,35 @@ public class PostCommandService {
     }
 
 
-    /* 2. 게시글 수정 */
+    /* 2. 게시글, 태그 수정 */
+    @Transactional
+    public void updatePost(Long postSeq, PostAndTagRequestDTO updatedPost) {
+
+        Post post = postRepository.findById(postSeq)
+                .orElseThrow(() -> new EntityNotFoundException("Post not found with postSeq: " + postSeq));
+
+        /* 수정을 위해 엔티티 정보 변경 */
+        post.updatePostDetails(
+                updatedPost.getPostTitle(),
+                updatedPost.getPostContent(),
+                updatedPost.getBoardCategorySeq(),
+                updatedPost.getListSeq(),
+                updatedPost.getRestaurantSeq()
+        );
+
+        // 수정 요청에 들어온 태그 리스트 저장
+        if (updatedPost.getTagSeq() != null) {
+
+            // 태그 등록 이전에 post_tag 테이블에 해당 게시글과 관련하여 등록된 태그 모두 삭제
+            postTagRepository.deleteAllByPostSeq(postSeq);
+
+            // 태그 등록
+            for (Long tagSeq : updatedPost.getTagSeq()) {
+                PostTag postTag = new PostTag(tagSeq, postSeq); // 게시글 고유번호와 함께 태그 저장
+                postTagRepository.save(postTag); // 게시글 태그를 DB에 저장
+            }
+        }
+    }
 
     /* 3. 게시글 삭제*/
     public Long deletePost(Long postSeq) {
@@ -53,7 +83,7 @@ public class PostCommandService {
         // 전달 된 postSeq로 Post Entity 조회
         Post post = postRepository.findById(postSeq)
                 // 조회 된 Post 엔티티가 없을 경우
-                .orElseThrow(() -> new EntityNotFoundException("Post not found with id: " + postSeq));
+                .orElseThrow(() -> new EntityNotFoundException("Post not found with postSeq: " + postSeq));
 
         // getter 메소드를 통해 반환해 줄 boardCategorySeq 값 삽입
         Long boardCategorySeq = post.getBoardCategorySeq();
@@ -64,6 +94,7 @@ public class PostCommandService {
         return boardCategorySeq;
 
     }
+
 
 
 }
