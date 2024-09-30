abfd347730b0da9b00d42909f8cf0ddda2160051
gim-yunhu <rlarjswn0303@gmail.com>
2024-09-30 23:46:06 +0900

    feat: #26 리스트 신고 기능 입력 DTO 추가

---
+++ matzipback/src/main/java/com/matzip/matzipback/report/command/dto/ListReportReqDTO.java
@@ -0,0 +1,16 @@
+package com.matzip.matzipback.report.command.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ListReportReqDTO {
+
+    private Long listSeq;
+    private String reportContent;
+
+
+}
