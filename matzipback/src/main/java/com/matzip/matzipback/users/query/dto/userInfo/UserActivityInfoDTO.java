206db127f521bef0ea51c3acb27d528adf047fe9
catnine11 <catnine11@gmail.com>
2024-09-30 11:12:32 +0900

    feat: 회원조회를 위한 활동도 관련 dto 생성

---
+++ matzipback/src/main/java/com/matzip/matzipback/users/query/dto/userInfo/UserActivityInfoDTO.java
@@ -0,0 +1,16 @@
+package com.matzip.matzipback.users.query.dto.userInfo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserActivityInfoDTO {
+
+    private long userSeq;
+    private long activeLevelSeq;
+    private String activeLevelName; // 등급 이름
+    private long activityPoint; // 활동 포인트
+    private int activeLevelStandard; // 등급 기준
+    private String influencerYn; // 인기회원 여부
+}
