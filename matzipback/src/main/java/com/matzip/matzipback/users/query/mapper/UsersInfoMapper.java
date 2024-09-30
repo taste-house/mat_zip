15440c087c486de5e538fb3c23c7ddb12bfb0398
catnine11 <catnine11@gmail.com>
2024-09-30 17:10:40 +0900

    feat: 소셜사이트 가입상태 필드 추가, 회원검색을 위한 메소드 정의

--- matzipback/src/main/java/com/matzip/matzipback/users/query/mapper/UsersInfoMapper.java
+++ matzipback/src/main/java/com/matzip/matzipback/users/query/mapper/UsersInfoMapper.java
@@ -10,14 +10,38 @@ import java.util.List;
 public interface UsersInfoMapper {
 
     List<UserInfoDTO> getAllUserInfo(@Param("socialYn") String socialYn,    // 명시적 매핑위해 @Param 으로 작성
+                                     @Param("socialSite") String socialSite,
                                      @Param("businessVerifiedYn") String businessVerifiedYn,
                                      @Param("influencerYn") String influencerYn,
+                                     @Param("userStatus") String userStatus,
                                      @Param("orderBy") String orderBy,
                                      @Param("size") Integer size,
                                      @Param("offset") int offset);
 
-    long getTotalUserCount(@Param("socialYn") String socialYn,    // 명시적 매핑위해 @Param 으로 작성
+    Long getTotalUserCount(@Param("socialYn") String socialYn,    // 명시적 매핑위해 @Param 으로 작성
+                           @Param("socialSite") String socialSite,
                            @Param("businessVerifiedYn") String businessVerifiedYn,
-                           @Param("influencerYn") String influencerYn);
+                           @Param("influencerYn") String influencerYn,
+                           @Param("userStatus") String userStatus);
 
+    List<UserInfoDTO> searchUserInfo(@Param("searchType") String searchType,
+                                     @Param("searchWord") String searchWord,
+                                     @Param("socialYn") String socialYn,
+                                     @Param("socialSite") String socialSite,
+                                     @Param("businessVerifiedYn") String businessVerifiedYn,
+                                     @Param("influencerYn") String influencerYn,
+                                     @Param("userStatus") String userStatus,
+                                     @Param("userAuth") String userAuth,
+                                     @Param("orderBy") String orderBy,
+                                     @Param("size") Integer size,
+                                     @Param("offset") int offset);
+
+    Long getSearchUserCount(@Param("searchType") String searchType,
+                            @Param("searchWord") String searchWord,
+                            @Param("socialYn") String socialYn,
+                            @Param("socialSite") String socialSite,
+                            @Param("businessVerifiedYn") String businessVerifiedYn,
+                            @Param("influencerYn") String influencerYn,
+                            @Param("userStatus") String userStatus,
+                            @Param("userAuth") String userAuth);
 }
