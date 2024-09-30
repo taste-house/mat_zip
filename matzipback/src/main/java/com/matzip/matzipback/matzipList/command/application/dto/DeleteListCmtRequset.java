b23f5ee87f500f8217ebdd359213b8e512365f0a
gim-yunhu <rlarjswn0303@gmail.com>
2024-09-30 12:27:46 +0900

    feat: #34 DTO 누락 추가

--- matzipback/src/main/java/com/matzip/matzipback/matzipList/command/application/dto/DeleteListCmtRequset.java
+++ matzipback/src/main/java/com/matzip/matzipback/matzipList/command/application/dto/DeleteListCmtRequset.java
@@ -1,4 +1,14 @@
 package com.matzip.matzipback.matzipList.command.application.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public class DeleteListCmtRequset {
+    private Long listSeq;
+    @NotBlank
+    private Long listCommentSeq;
+    private Long listCommentListSeq;
 }
