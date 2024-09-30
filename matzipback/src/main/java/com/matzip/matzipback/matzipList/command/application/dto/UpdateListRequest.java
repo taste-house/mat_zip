b8d4753c36f596eb03e7ee65b7c0e7f60c9b9651
gim-yunhu <rlarjswn0303@gmail.com>
2024-10-01 00:43:12 +0900

    fix: #207 리스트 수정 기능 DTO 수정

--- matzipback/src/main/java/com/matzip/matzipback/matzipList/command/application/dto/UpdateListRequest.java
+++ matzipback/src/main/java/com/matzip/matzipback/matzipList/command/application/dto/UpdateListRequest.java
@@ -1,5 +1,7 @@
 package com.matzip.matzipback.matzipList.command.application.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
@@ -8,10 +10,11 @@ import lombok.Setter;
 @Setter
 @RequiredArgsConstructor
 public class UpdateListRequest {
+    @NotNull
     private Long listSeq;
-    private Long listUserSeq;
+    @NotNull
     private String listTitle;
+    @NotBlank
     private String listContent;
-    private Integer listLevel;
 
 }
