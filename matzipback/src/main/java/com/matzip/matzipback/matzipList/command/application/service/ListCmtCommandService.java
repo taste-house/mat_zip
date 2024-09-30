352e0eab0f3f0b97ed6385e381a58229dc32fd91
gim-yunhu <rlarjswn0303@gmail.com>
2024-10-01 00:46:40 +0900

    fix: #207 리스트 댓글 파일명 변경으로 인한 서비스 코드 수정

--- matzipback/src/main/java/com/matzip/matzipback/matzipList/command/application/service/ListCmtCommandService.java
+++ matzipback/src/main/java/com/matzip/matzipback/matzipList/command/application/service/ListCmtCommandService.java
@@ -1,13 +1,11 @@
 package com.matzip.matzipback.matzipList.command.application.service;
 
-import com.matzip.matzipback.common.util.CustomUserUtils;
 import com.matzip.matzipback.matzipList.command.application.dto.CreateListCmtRequest;
-import com.matzip.matzipback.matzipList.command.application.dto.DeleteListCmtRequset;
+import com.matzip.matzipback.matzipList.command.application.dto.DeleteListCmtRequest;
 import com.matzip.matzipback.matzipList.command.application.dto.UpdateListCmtRequest;
 import com.matzip.matzipback.matzipList.command.domain.aggregate.MyListComment;
 import com.matzip.matzipback.matzipList.command.domain.repository.ListCmtDomainRepository;
 import com.matzip.matzipback.matzipList.command.domain.service.DomainListCmtUpdateService;
-import com.matzip.matzipback.matzipList.command.domain.service.DomainListUpdateService;
 import com.matzip.matzipback.matzipList.command.mapper.ListCmtMapper;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
@@ -39,8 +37,8 @@ public class ListCmtCommandService {
 
     // 리스트 댓글 삭제
     @Transactional
-    public void deleteListCmt(DeleteListCmtRequset deleteListCmtRequset) {
-        listCmtDomainRepository.deleteById(deleteListCmtRequset.getListCommentSeq());
+    public void deleteListCmt(DeleteListCmtRequest deleteListCmtRequest) {
+        listCmtDomainRepository.deleteById(deleteListCmtRequest.getListCommentSeq());
     }
 
     // 리스트 댓글 수정
