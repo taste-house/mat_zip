36afa5f55f6a39f248ed1b69c93a38cdeb12effc
gim-yunhu <rlarjswn0303@gmail.com>
2024-09-30 16:01:32 +0900

    chore: #201 리스트 좋아요 등록/취소 기능 DTO 파일명 수정

--- matzipback/src/main/java/com/matzip/matzipback/like/command/application/dto/ListLikeMessageReqDTO.java
+++ matzipback/src/main/java/com/matzip/matzipback/like/command/application/dto/ListLikeMessageReqDTO.java
@@ -7,7 +7,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @AllArgsConstructor
-public class ListLikeMessageReqDto {
+public class ListLikeMessageReqDTO {
 
     private int code;
     private String message;
