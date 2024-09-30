89dc1cd418ab298d673dcc0a9535cfe622bd5081
gim-yunhu <rlarjswn0303@gmail.com>
2024-10-01 00:46:48 +0900

    fix: #207 리스트 댓글 파일명 변경으로 인한 레포 코드 수정

--- matzipback/src/main/java/com/matzip/matzipback/matzipList/command/domain/repository/ListCmtDomainRepository.java
+++ matzipback/src/main/java/com/matzip/matzipback/matzipList/command/domain/repository/ListCmtDomainRepository.java
@@ -1,6 +1,5 @@
 package com.matzip.matzipback.matzipList.command.domain.repository;
 
-import com.matzip.matzipback.matzipList.command.application.dto.DeleteListCmtRequset;
 import com.matzip.matzipback.matzipList.command.domain.aggregate.MyListComment;
 import jakarta.validation.constraints.NotBlank;
 
