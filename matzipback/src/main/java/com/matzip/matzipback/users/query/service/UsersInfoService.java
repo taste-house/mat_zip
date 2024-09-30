eb225ab394e9deee7e6394ab68eb0683fad5dea6
catnine11 <catnine11@gmail.com>
2024-09-30 17:10:57 +0900

    feat: 소셜사이트 가입상태 필드 추가, 회원검색 구현

--- matzipback/src/main/java/com/matzip/matzipback/users/query/service/UsersInfoService.java
+++ matzipback/src/main/java/com/matzip/matzipback/users/query/service/UsersInfoService.java
@@ -16,24 +16,25 @@ public class UsersInfoService {
 
     private final UsersInfoMapper usersInfoMapper;
 
-    public AllUserInfoResponseDTO getAllUserList(String socialYn, String businessVerifiedYn, String influencerYn,    // 필터링 조건
-                                                 String orderBy, Integer page, Integer size) {   // 정렬조건, 페이징
-        log.info("getAllUserList() 호출 - 필터링 조건: socialYn={}, businessVerifiedYn={}, influencerYn={}, orderBy={}",
-                socialYn, businessVerifiedYn, influencerYn, orderBy);
+    public AllUserInfoResponseDTO getAllUserList(String socialYn, String socialSite, String businessVerifiedYn,
+                                                 String influencerYn, String userStatus, String orderBy,   // 필터링 조건, 정렬
+                                                 Integer page, Integer size) {   // 페이징
+        log.info("getAllUserList() 호출 - 필터링 조건: socialYn={}, businessVerifiedYn={}, influencerYn={}, userStatus={}, orderBy={}",
+                socialYn, businessVerifiedYn, influencerYn, userStatus, orderBy);
 
         // 페이지 번호에 따른 offset 계산(offset : 데이터를 건너뛰는 개수)
         int offset = (page -1) * size;
 
         // 총 유저 수
-        long totalUsers = usersInfoMapper.getTotalUserCount(socialYn, businessVerifiedYn, influencerYn);
+        Long totalUsers = usersInfoMapper.getTotalUserCount(socialYn, socialSite, businessVerifiedYn, influencerYn, userStatus);
 
         // 전체 페이지 수 계산
         int totalPages = (int) Math.ceil((double) totalUsers / size);
 
         // 페이징 된 유저 리스트 가져오기
-        List<UserInfoDTO> userList = usersInfoMapper.getAllUserInfo(socialYn, businessVerifiedYn, influencerYn, orderBy, size, offset);
+        List<UserInfoDTO> userList = usersInfoMapper.getAllUserInfo(socialYn, socialSite, businessVerifiedYn, influencerYn, userStatus, orderBy, size, offset);
 
-        log.info("getAllUsers() 완료 - 조회된 회원 수: {}", userList.size());
+        log.info("getAllUsers() 완료 - 페이지에 조회된 회원 수: {}, 총 회원 수 : {}", userList.size(), totalUsers);
 
 //        AllUserInfoResponseDTO allUserInfoResponseDTO = new AllUserInfoResponseDTO(userList, page, totalPages, totalUsers);
 //        return allUserInfoResponseDTO;
@@ -46,4 +47,35 @@ public class UsersInfoService {
     }
 
 
+    public AllUserInfoResponseDTO getSearchUserList(String searchType, String searchWord,   // 검색조건
+                                                    String socialYn, String socialSite, String businessVerifiedYn,
+                                                    String influencerYn, String userStatus,  String userAuth, String orderBy,   // 필터링 조건, 정렬
+                                                    Integer page, Integer size) {   // 페이징
+        log.info("getSearchUserList() 호출 - 검색 조건: searchType={}, searchWord={}", searchType, searchWord);
+
+        // 페이지 번호에 따른 offset 계산(offset : 데이터를 건너뛰는 개수)
+        int offset = (page -1) * size;
+
+        // 유저 검색결과 수
+        Long totalUsers = usersInfoMapper.getSearchUserCount(searchType, searchWord,
+                socialYn, socialSite, businessVerifiedYn, influencerYn, userStatus, userAuth);
+
+        // 전체 페이지 수 계산
+        int totalPages = (int) Math.ceil((double) totalUsers / size);
+
+        // 페이징 된 유저 리스트 가져오기
+        List<UserInfoDTO> userList = usersInfoMapper.searchUserInfo(searchType, searchWord,
+                socialYn, socialSite, businessVerifiedYn, influencerYn, userStatus, userAuth, orderBy, size, offset);
+
+        log.info("getSearchUserList() 완료 - 페이지에 검색된 회원 수: {}, 총 회원 수 : {}", userList.size(), totalUsers);
+
+//        AllUserInfoResponseDTO allUserInfoResponseDTO = new AllUserInfoResponseDTO(userList, page, totalPages, totalUsers);
+//        return allUserInfoResponseDTO;
+        return AllUserInfoResponseDTO.builder()
+                .userLists(userList)
+                .currentPage(page)
+                .totalPages(totalPages)
+                .totalUsers(totalUsers)
+                .build();
+    }
 }
