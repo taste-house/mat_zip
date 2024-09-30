5c0b730a12e5ca3c23c7baf2599ab68668af0dc3
leebackcoding <lcy2825@gmail.com>
2024-09-30 14:29:04 +0900

    chore: 쪽지방 목록 조회 mapper 추가

---
+++ matzipback/src/main/java/com/matzip/matzipback/users/query/mapper/MessageMapper.java
@@ -0,0 +1,11 @@
+package com.matzip.matzipback.users.query.mapper;
+
+import com.matzip.matzipback.users.query.dto.MessageChatResDTO;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface MessageMapper {
+    List<MessageChatResDTO> searchMessageListByUserSeq(long loginUserSeq);
+}
