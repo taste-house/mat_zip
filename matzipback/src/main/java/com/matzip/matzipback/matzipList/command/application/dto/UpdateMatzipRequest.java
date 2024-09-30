e639086768349aa2de869a9dacc414acf06e41ef
gim-yunhu <rlarjswn0303@gmail.com>
2024-09-30 11:27:59 +0900

    feat: #190 리스트 맛집 DTO 추가

--- matzipback/src/main/java/com/matzip/matzipback/matzipList/command/application/dto/UpdateMatzipRequest.java
+++ matzipback/src/main/java/com/matzip/matzipback/matzipList/command/application/dto/UpdateMatzipRequest.java
@@ -1,4 +1,19 @@
 package com.matzip.matzipback.matzipList.command.application.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
 public class UpdateMatzipRequest {
+    @NotBlank
+    private final Long listSeq;
+    @NotBlank
+    private final Long listMatzipSeq;
+    @NotBlank
+    private final Long restaurantSeq;
+    private final String listMatzipComment;
 }
