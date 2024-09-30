dedca867f8aba24b8a73a8a540c845587d17bf3c
gim-yunhu <rlarjswn0303@gmail.com>
2024-10-01 00:31:43 +0900

    fix: #207 맛집 DTO 파일명 변경

--- matzipback/src/main/java/com/matzip/matzipback/matzipList/command/application/dto/DeleteMatzipRequest.java
+++ matzipback/src/main/java/com/matzip/matzipback/matzipList/command/application/dto/DeleteMatzipRequest.java
@@ -6,7 +6,7 @@ import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
-public class DeleteMatzipRequset {
+public class DeleteMatzipRequest {
 
     @NotBlank
     private Long listMatzipSeq;
