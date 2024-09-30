0c41bf18ed703cd36deb3a3904f898b1746044bb
JIYOUNG-22 <dyong0922@naver.com>
2024-09-30 17:56:41 +0900

    chore: 필드명 수정

--- matzipback/src/main/java/com/matzip/matzipback/board/query/dto/PostListResponse.java
+++ matzipback/src/main/java/com/matzip/matzipback/board/query/dto/PostListResponse.java
@@ -8,7 +8,7 @@ import java.util.List;
 @Getter
 @Builder
 public class PostListResponse {
-    private List<PostDto> posts;    // 검색조건에 부합하는 게시글 리스트
+    private List<PostDTO> posts;    // 검색조건에 부합하는 게시글 리스트
     private int currentPage;        // 현재 페이지
     private int totalPages;         // 전체 페이지 수
     private long totalPosts;        // 검색조건에 부합하는 총 게시글 수
