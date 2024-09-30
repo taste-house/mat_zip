06f1b3d102939c3c53c056c016a06f054f6315f2
gim-yunhu <rlarjswn0303@gmail.com>
2024-10-01 01:29:00 +0900

    fix: #209 리스트 삭제 기능 레포 코드 추가

--- matzipback/src/main/java/com/matzip/matzipback/matzipList/command/domain/repository/ListDomainRepository.java
+++ matzipback/src/main/java/com/matzip/matzipback/matzipList/command/domain/repository/ListDomainRepository.java
@@ -1,8 +1,11 @@
 package com.matzip.matzipback.matzipList.command.domain.repository;
 
 import com.matzip.matzipback.board.command.domain.aggregate.Post;
+import com.matzip.matzipback.matzipList.command.application.dto.DeleteListRequest;
 import com.matzip.matzipback.matzipList.command.domain.aggregate.MyList;
 import com.matzip.matzipback.users.command.domain.aggregate.Users;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -12,12 +15,11 @@ import java.util.Optional;
 
 public interface ListDomainRepository {
 
-
     MyList save(MyList myList);
 
     long countByListUserSeq(Long listUserSeq);
 
-    void deleteById(Long listSeq);
+    void deleteById(@NotNull Long listSeq);
 
     Optional<MyList> findById(Long listSeq);
 
