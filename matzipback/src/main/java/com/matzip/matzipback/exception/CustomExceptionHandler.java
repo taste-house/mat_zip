3f7e22f39f1c2aa574d066cc35c01729f5b80d28
leebackcoding <lcy2825@gmail.com>
2024-10-01 15:40:20 +0900

    feat: CustomExceptionHandler 추가

---
+++ matzipback/src/main/java/com/matzip/matzipback/exception/CustomExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.matzip.matzipback.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@RestControllerAdvice
+public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(RestApiException.class)
+    protected ResponseEntity<ErrorResponse> handleExceptionInternal(RestApiException e) {
+        ErrorCode errorCode = e.getErrorCode();
+        return handleExceptionInternal(errorCode);
+    }
+
+    private ResponseEntity<ErrorResponse> handleExceptionInternal(ErrorCode errorCode) {
+        return ResponseEntity
+                .status(errorCode.getHttpStatus())
+                .body(new ErrorResponse(errorCode));
+    }
+}
