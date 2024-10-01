96005ccfc8c3a52a5f156cc3a755c3c843386dec
leebackcoding <lcy2825@gmail.com>
2024-10-01 23:32:13 +0900

    refactoring: 게시글 좋아요 1차 수정

--- matzipback/src/main/java/com/matzip/matzipback/like/command/application/controller/PostLikeController.java
+++ matzipback/src/main/java/com/matzip/matzipback/like/command/application/controller/PostLikeController.java
@@ -1,14 +1,15 @@
 package com.matzip.matzipback.like.command.application.controller;
 
 import com.matzip.matzipback.like.command.application.dto.PostLikeReqDTO;
-import com.matzip.matzipback.like.command.application.dto.PostLikeResMessageDTO;
 import com.matzip.matzipback.like.command.application.service.PostLikeService;
-import com.matzip.matzipback.like.command.domain.aggregate.Like;
-import com.matzip.matzipback.responsemessage.ResponseMessage;
+import com.matzip.matzipback.responsemessage.SuccessCode;
+import com.matzip.matzipback.responsemessage.SuccessResMessage;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,17 +18,17 @@ public class PostLikeController {
 
     private final PostLikeService postLikeService;
 
-
+    // 1차 수정완료 - 창윤
     @PostMapping("/post/like")
-    public ResponseEntity<PostLikeResMessageDTO> savePostLike(@RequestBody PostLikeReqDTO postLikeReqDTO) {
-        Like resultLike = postLikeService.savePostLike(postLikeReqDTO);
+    public ResponseEntity<SuccessResMessage> savePostLike(@RequestBody PostLikeReqDTO postLikeReqDTO) {
+        boolean resultLike = postLikeService.savePostLike(postLikeReqDTO);
 
-        if (resultLike != null) {
+        if (resultLike) {
             // 좋아요 등록
-            return ResponseEntity.status(HttpStatus.CREATED).body(new PostLikeResMessageDTO(201, ResponseMessage.SAVE_SUCCESS.getMessage(), resultLike.getPostSeq()));
+            return ResponseEntity.ok(new SuccessResMessage(SuccessCode.LIKE_SUCCESS));
         }
 
         // 좋아요 취소
-        return ResponseEntity.status(HttpStatus.OK).body(new PostLikeResMessageDTO(200, ResponseMessage.SAVE_FAIL.getMessage(), -1));
+        return ResponseEntity.ok(new SuccessResMessage(SuccessCode.LIKE_DELETE_SUCCESS));
     }
 }
