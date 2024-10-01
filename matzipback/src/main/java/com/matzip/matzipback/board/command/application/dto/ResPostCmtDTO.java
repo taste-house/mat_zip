ebb2457fb1f6708798d53aea2ffa5bcbfc3ddad4
wildcat222 <wildcat222@naver.com>
2024-10-02 01:34:06 +0900

    chore: 댓글 작성 응답 DTO 수정

--- matzipback/src/main/java/com/matzip/matzipback/board/command/application/dto/ResPostCmtDTO.java
+++ matzipback/src/main/java/com/matzip/matzipback/board/command/application/dto/ResPostCmtDTO.java
@@ -2,16 +2,16 @@ package com.matzip.matzipback.board.command.application.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @AllArgsConstructor
-@NoArgsConstructor
 public class ResPostCmtDTO {
 
-    private int code;
-    private String message;
-    private Long postSeq;
+    private Long postCommentSeq;
+    private LocalDateTime postCommentCreatedTime;
+
 }
