d8c427c14ebaa7b59f5a568f2ba3625efab4b69b
leebackcoding <lcy2825@gmail.com>
2024-10-02 00:14:47 +0900

    refactoring: 좋아요 처리에 대한 성공 메시지 추가

--- matzipback/src/main/java/com/matzip/matzipback/responsemessage/SuccessCode.java
+++ matzipback/src/main/java/com/matzip/matzipback/responsemessage/SuccessCode.java
@@ -10,7 +10,15 @@ public enum SuccessCode {
 
     BASIC_SAVE_SUCCESS(HttpStatus.CREATED, "저장 성공했습니다."),
     BASIC_UPDATE_SUCCESS(HttpStatus.NO_CONTENT, "수정 성공했습니다."),
-    BASIC_DELETE_SUCCESS(HttpStatus.NO_CONTENT, "삭제 성공했습니다.");
+    BASIC_DELETE_SUCCESS(HttpStatus.NO_CONTENT, "삭제 성공했습니다."),
+
+    /* 좋아요 등록 */
+    LIKE_SUCCESS(HttpStatus.OK, "좋아요 성공"),
+
+    /* 좋아요 취소 */
+    LIKE_DELETE_SUCCESS(HttpStatus.OK, "좋아요 취소");
+
+
 
     private final HttpStatus httpStatus;
     private final String message;
