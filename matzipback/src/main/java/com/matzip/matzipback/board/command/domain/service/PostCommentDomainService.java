5ab5d40ea2a7d602fcbf3e0f2a1e9103437ff2f7
wildcat222 <wildcat222@naver.com>
2024-10-02 00:10:43 +0900

    fix: DomainService 댓글 수정 메서드 수정 및 예외 처리 로직 추가

--- matzipback/src/main/java/com/matzip/matzipback/board/command/domain/service/PostCommentDomainService.java
+++ matzipback/src/main/java/com/matzip/matzipback/board/command/domain/service/PostCommentDomainService.java
@@ -1,5 +1,6 @@
 package com.matzip.matzipback.board.command.domain.service;
 
+import com.matzip.matzipback.board.command.application.dto.ReqPostCmtUpdateDTO;
 import com.matzip.matzipback.board.command.domain.aggregate.PostComment;
 import com.matzip.matzipback.board.command.domain.repository.PostCommentRepository;
 import com.matzip.matzipback.exception.ErrorCode;
@@ -21,6 +22,16 @@ public class PostCommentDomainService {
                 .orElseThrow(NoSuchElementException::new);
     }
 
+    // 댓글 수정 후 저장
+    public void updatePostCmt(ReqPostCmtUpdateDTO reqPostCmtUpdateDTO) {
+        // 스프링 jpa를 이용해서 영속성 컨텍스트로 해당 댓글 가져오기
+        PostComment postComment = postCommentRepository.findById(reqPostCmtUpdateDTO.getPostCommentSeq())
+                .orElseThrow(() -> new RestApiException(ErrorCode.NOT_FOUND));
+
+        // 수정 값이 업데이트 시에 자동으로 @LastModifiedDate 적용
+        postComment.updatePostCmt(reqPostCmtUpdateDTO);
+    }
+
     // postCommentSeq 로 조회 후 작성자 userSeq 로 반환
     public Long findUserSeqByPostCmtSeq(Long postCommentSeq) {
         PostComment postComment = postCommentRepository.findById(postCommentSeq)
@@ -32,4 +43,6 @@ public class PostCommentDomainService {
     public void deletePostCmtById(Long postCommentSeq) {
         postCommentRepository.deleteById(postCommentSeq);
     }
+
+
 }
