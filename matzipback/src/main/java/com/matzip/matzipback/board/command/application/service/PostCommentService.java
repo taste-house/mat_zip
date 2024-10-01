afed6b4280f53a5a6949c94bc8ae2761a6312a31
wildcat222 <wildcat222@naver.com>
2024-10-02 00:52:18 +0900

    fix: Service 댓글 작성 로직 수정

--- matzipback/src/main/java/com/matzip/matzipback/board/command/application/service/PostCommentService.java
+++ matzipback/src/main/java/com/matzip/matzipback/board/command/application/service/PostCommentService.java
@@ -22,23 +22,13 @@ public class PostCommentService {
 
     // 댓글 작성
     @Transactional
-    public PostComment createPostComment(ReqPostCmtCreateDTO reqPostCmtCreateDTO) {
+    public void createPostComment(ReqPostCmtCreateDTO reqPostCmtCreateDTO) {
         // 나중에 Authorization 에서 빼와야한다. JwtUtil 에서의 메서드 활용할 것임
-        Long userSeq = 1L;
+        Long userSeq = /*CustomUserUtils.getCurrentUserSeq();*/ 1L;
 
-        // DTO -> Entity 매퍼를 사용하여 생성되도록 수정 (Entity 불변성 유지)
-//        PostComment postComment = PostComment.create(reqPostCmtCreateDTO, userSeq);
         reqPostCmtCreateDTO.setPostCommentUserSeq(userSeq);
-        PostComment postComment = modelMapper.map(reqPostCmtCreateDTO, PostComment.class);
 
-        // 유효성 검사 (댓글 내용이 작성되어있어야 한다.)
-        // null 체크 및 처리
-        if (postComment.getPostCommentContent() == null || postComment.getPostCommentContent().isEmpty()) {
-//            throw new RuntimeException("댓글 저장에 실패했습니다."); // 예외 처리
-            return postComment;
-        }
-
-        return postCommentRepository.save(postComment); // 댓글 저장 후 Entity 반환
+        postCommentDomainService.save(reqPostCmtCreateDTO);
     }
 
     // 댓글 수정
