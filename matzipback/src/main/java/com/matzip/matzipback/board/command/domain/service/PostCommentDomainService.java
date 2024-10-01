4ef8c04b9e820d6cc51e6b55c9c8d90656904995
wildcat222 <wildcat222@naver.com>
2024-10-01 22:52:42 +0900

    fix: DomainService postCommentSeq 로 조회 후 작성자 userSeq 로 반환하는 메서드 추가 및 예외 처리

--- matzipback/src/main/java/com/matzip/matzipback/board/command/domain/service/PostCommentDomainService.java
+++ matzipback/src/main/java/com/matzip/matzipback/board/command/domain/service/PostCommentDomainService.java
@@ -2,8 +2,12 @@ package com.matzip.matzipback.board.command.domain.service;
 
 import com.matzip.matzipback.board.command.domain.aggregate.PostComment;
 import com.matzip.matzipback.board.command.domain.repository.PostCommentRepository;
+import com.matzip.matzipback.exception.ErrorCode;
+import com.matzip.matzipback.exception.RestApiException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
 
 @Service
 @RequiredArgsConstructor
@@ -13,7 +17,15 @@ public class PostCommentDomainService {
 
     // postCommentSeq 로 postComment 조회
     public PostComment findByPostCommentSeq(Long postCommentSeq) {
-        return postCommentRepository.findById(postCommentSeq).orElse(null);
+        return postCommentRepository.findById(postCommentSeq)
+                .orElseThrow(NoSuchElementException::new);
+    }
+
+    // postCommentSeq 로 조회 후 작성자 userSeq 로 반환
+    public Long findUserSeqByPostCmtSeq(Long postCommentSeq) {
+        PostComment postComment = postCommentRepository.findById(postCommentSeq)
+                .orElseThrow(() -> new RestApiException(ErrorCode.NOT_FOUND));
+        return postComment.getPostCommentUserSeq();
     }
 
     // postCommentSeq 로 deletebById
