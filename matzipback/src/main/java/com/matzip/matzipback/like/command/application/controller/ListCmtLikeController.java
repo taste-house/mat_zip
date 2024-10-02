407ad2784300a799500d2a7fbf05d37ae3d4a94c
leebackcoding <lcy2825@gmail.com>
2024-10-02 10:20:38 +0900

    refactoring: 리스트 댓글 좋아요 controller 리팩토링

--- matzipback/src/main/java/com/matzip/matzipback/like/command/application/controller/ListCmtLikeController.java
+++ matzipback/src/main/java/com/matzip/matzipback/like/command/application/controller/ListCmtLikeController.java
@@ -1,13 +1,11 @@
 package com.matzip.matzipback.like.command.application.controller;
 
-import com.matzip.matzipback.like.command.application.dto.ListCmtLikeMessageDTO;
 import com.matzip.matzipback.like.command.application.dto.ListCmtLikeReqDTO;
 import com.matzip.matzipback.like.command.application.service.ListCmtLikeService;
-import com.matzip.matzipback.like.command.domain.aggregate.Like;
-import com.matzip.matzipback.responsemessage.ResponseMessage;
+import com.matzip.matzipback.responsemessage.SuccessCode;
+import com.matzip.matzipback.responsemessage.SuccessResMessage;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,15 +19,18 @@ public class ListCmtLikeController {
 
     private final ListCmtLikeService listCmtLikeService;
 
+    // 1차 수정 - 창윤
     @PostMapping("/listCmt/like")
-    public ResponseEntity<ListCmtLikeMessageDTO> saveListCmtLike(@Valid @RequestBody ListCmtLikeReqDTO listCmtLikeRequest){
-        Like resultLike = listCmtLikeService.saveAndDeleteListCmtLike(listCmtLikeRequest);
+    public ResponseEntity<SuccessResMessage> saveListCmtLike(@Valid @RequestBody ListCmtLikeReqDTO listCmtLikeRequest){
+        boolean resultLike = listCmtLikeService.saveAndDeleteListCmtLike(listCmtLikeRequest);
 
-        if (resultLike != null) {
-            return ResponseEntity.status(HttpStatus.CREATED).body(new ListCmtLikeMessageDTO(201, ResponseMessage.SAVE_SUCCESS.getMessage(), listCmtLikeRequest.getListCommentSeq()));
+        // 좋아요 등록
+        if (resultLike) {
+            return ResponseEntity.ok(new SuccessResMessage(SuccessCode.LIKE_SUCCESS));
         }
 
-        return ResponseEntity.status(HttpStatus.OK).body(new ListCmtLikeMessageDTO(200, ResponseMessage.SAVE_FAIL.getMessage(), listCmtLikeRequest.getListCommentSeq()));
+        // 좋아요 취소
+        return ResponseEntity.ok(new SuccessResMessage(SuccessCode.LIKE_DELETE_SUCCESS));
     }
 
 }
