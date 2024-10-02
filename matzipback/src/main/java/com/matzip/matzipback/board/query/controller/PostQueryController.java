3fab6b65ead15ea876a4bccd49cac2637336e189
JIYOUNG-22 <dyong0922@naver.com>
2024-10-02 15:09:27 +0900

    feat: 인기태그 제시 기능 구현을 위한 PostQueryController 추가

--- matzipback/src/main/java/com/matzip/matzipback/board/query/controller/PostQueryController.java
+++ matzipback/src/main/java/com/matzip/matzipback/board/query/controller/PostQueryController.java
@@ -19,7 +19,7 @@ public class PostQueryController {
     private final PostQueryService postQueryService;
 
     /* 1. 검색조건(게시글 제목, 게시글 작성자 닉네임)에 맞는 게시글 목록 조회 */
-    @GetMapping("/posts/search")
+    @GetMapping("/posts")
     @Operation(summary = "게시글 검색", description = "게시글 제목, 작성자 닉네임으로 게시글을 검색한다.")
     public ResponseEntity<PostListResponse> searchPosts(
             // paging 처리
@@ -34,14 +34,14 @@ public class PostQueryController {
     }
 
     /* 2. 게시판 카테고리 별 게시글 목록 조회 */
-    @GetMapping("/category/{boardCategorySeq}/posts")
+    @GetMapping("/boards/{boardSeq}/posts")
     @Operation(summary = "게시판 내 게시글 목록 조회", description = "게시판 카테고리 별로 게시글 목록을 조회한다.")
     public ResponseEntity<PostListResponse> getPostsByCategory(
             @RequestParam(defaultValue = "1") Integer page,
             @RequestParam(defaultValue = "10") Integer size,
-            @PathVariable Long boardCategorySeq
+            @PathVariable Long boardSeq
     ) {
-        PostListResponse response = postQueryService.getPostsByCategory(page, size, boardCategorySeq);
+        PostListResponse response = postQueryService.getPostsByCategory(page, size, boardSeq);
 
         return ResponseEntity.ok(response);
     }
@@ -57,12 +57,23 @@ public class PostQueryController {
     }
 
     /* 4. 게시판 별 인기 태그 조회 */
-    @GetMapping("/boards/{boardSeq}/popular-tag")
+    @GetMapping("/boards/{boardSeq}/popular-tags")
     @Operation(summary = "게시판 인기 태그 조회", description = "게시판 카테고리 별로 인기 태그를 조회한다.")
     public ResponseEntity<PopularTagResponse> getPopularTag(@PathVariable Long boardSeq) {
 
         PopularTagResponse response = postQueryService.getPopularTag(boardSeq);
 
         return ResponseEntity.ok(response);
+    }
+
+    /* 5. 게시글 등록 페이지에서 태그 작성 시 관련 인기 태그(키워드) 제시 */
+    @GetMapping("/posts/tags")
+    @Operation(summary = "관련 인기 태그 제시", description = "게시글 등록 페이지에서 태그 작성 시 관련 인기 키워드를 볼 수 있다.")
+    public ResponseEntity<PopularTagResponse> getTagKeywords(
+            @RequestParam(required = false) String tag     // 사용자가 작성한 태그 키워드
+    ) {
+        PopularTagResponse response = postQueryService.getTagKeywords(tag);
+
+         return ResponseEntity.ok(response);
     }
 }
