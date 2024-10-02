7fbf36d6dc4e0f01a237b5d0fc20ef26fb335eb7
catnine11 <catnine11@gmail.com>
2024-10-02 23:27:46 +0900

    chore: 스웨거를 위한 어노테이션 추가

--- matzipback/src/main/java/com/matzip/matzipback/users/command/application/controller/EmailController.java
+++ matzipback/src/main/java/com/matzip/matzipback/users/command/application/controller/EmailController.java
@@ -3,6 +3,8 @@ package com.matzip.matzipback.users.command.application.controller;
 import com.matzip.matzipback.users.command.application.service.EmailService;
 import com.matzip.matzipback.users.command.dto.EmailChkRequest;
 import com.matzip.matzipback.users.command.dto.EmailSendRequest;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,11 +16,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1")
 @RequiredArgsConstructor
+@Tag(name = "Users", description = "회원관리")
 public class EmailController {
 
     private final EmailService emailService;
 
     @PostMapping("/auth/mail-verification")
+    @Operation(summary = "이메일 인증", description = "입력받은 이메일로 인증코드를 발송한다.")
     public ResponseEntity<String> sendVerificationCode(@RequestBody EmailSendRequest request) {
         emailService.sendSignUpEmail(request.getUserEmail(), request.getUserName());
 
@@ -26,6 +30,7 @@ public class EmailController {
     }
 
     @PostMapping("/auth/chkEmailCode")
+    @Operation(summary = "이메일 인증검증", description = "인증코드가 일치하는지 검증한다.")
     public ResponseEntity<String> checkVerifyCode(@RequestBody EmailChkRequest request) {
         boolean isVerified = emailService.verifyEmailCode(request.getUserEmail(), request.getVerificationCode());
         if (isVerified) {
