3420f08e2c6798df0c292783d7100873f0f13e8e
JIYOUNG-22 <dyong0922@naver.com>
2024-10-02 11:02:57 +0900

    chore: swagger 그룹화 작업

--- matzipback/src/main/java/com/matzip/matzipback/board/query/controller/PostQueryController.java
+++ matzipback/src/main/java/com/matzip/matzipback/board/query/controller/PostQueryController.java
@@ -4,6 +4,8 @@ import com.matzip.matzipback.board.query.dto.PopularTagResponse;
 import com.matzip.matzipback.board.query.dto.PostDetailResponse;
 import com.matzip.matzipback.board.query.dto.PostListResponse;
 import com.matzip.matzipback.board.query.service.PostQueryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,12 +13,14 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1")
+@Tag(name = "Post", description = "게시글")
 public class PostQueryController {
 
     private final PostQueryService postQueryService;
 
     /* 1. 검색조건(게시글 제목, 게시글 작성자 닉네임)에 맞는 게시글 목록 조회 */
     @GetMapping("/posts/search")
+    @Operation(summary = "게시글 검색", description = "게시글 제목, 작성자 닉네임으로 게시글을 검색한다.")
     public ResponseEntity<PostListResponse> searchPosts(
             // paging 처리
             @RequestParam(defaultValue = "1") Integer page,
@@ -31,6 +35,7 @@ public class PostQueryController {
 
     /* 2. 게시판 카테고리 별 게시글 목록 조회 */
     @GetMapping("/category/{boardCategorySeq}/posts")
+    @Operation(summary = "게시판 내 게시글 목록 조회", description = "게시판 카테고리 별로 게시글 목록을 조회한다.")
     public ResponseEntity<PostListResponse> getPostsByCategory(
             @RequestParam(defaultValue = "1") Integer page,
             @RequestParam(defaultValue = "10") Integer size,
@@ -43,6 +48,7 @@ public class PostQueryController {
 
     /* 3. 게시글 상세 조회 */
     @GetMapping("/posts/{postSeq}")
+    @Operation(summary = "게시글 상세 조회", description = "게시글을 상세 조회한다.")
     public ResponseEntity<PostDetailResponse> getPostDetail(@PathVariable Long postSeq) {
 
         PostDetailResponse response = postQueryService.getPostDetail(postSeq);
@@ -52,6 +58,7 @@ public class PostQueryController {
 
     /* 4. 게시판 별 인기 태그 조회 */
     @GetMapping("/boards/{boardSeq}/popular-tag")
+    @Operation(summary = "게시판 인기 태그 조회", description = "게시판 카테고리 별로 인기 태그를 조회한다.")
     public ResponseEntity<PopularTagResponse> getPopularTag(@PathVariable Long boardSeq) {
 
         PopularTagResponse response = postQueryService.getPopularTag(boardSeq);
