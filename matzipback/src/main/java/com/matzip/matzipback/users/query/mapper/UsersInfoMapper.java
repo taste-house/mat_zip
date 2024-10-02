55b45348e43f2f04e113bf4f63403906d79d3c7e
catnine11 <catnine11@gmail.com>
2024-10-02 23:40:17 +0900

    feat: 회원 상세조회 구현을 위해 매퍼에 메소드 정의

--- matzipback/src/main/java/com/matzip/matzipback/users/query/mapper/UsersInfoMapper.java
+++ matzipback/src/main/java/com/matzip/matzipback/users/query/mapper/UsersInfoMapper.java
@@ -1,5 +1,9 @@
 package com.matzip.matzipback.users.query.mapper;
 
+import com.matzip.matzipback.report.query.dto.PenaltyDTO;
+import com.matzip.matzipback.users.query.dto.userInfo.BusinessLicenseDTO;
+import com.matzip.matzipback.users.query.dto.userInfo.OtherUserInfoDto;
+import com.matzip.matzipback.users.query.dto.userInfo.UserDetailInfoDTO;
 import com.matzip.matzipback.users.query.dto.userInfo.UserInfoDTO;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
@@ -9,6 +13,7 @@ import java.util.List;
 @Mapper
 public interface UsersInfoMapper {
 
+    // 관리자의 전체조회
     List<UserInfoDTO> getAllUserInfo(@Param("socialYn") String socialYn,    // 명시적 매핑위해 @Param 으로 작성
                                      @Param("socialSite") String socialSite,
                                      @Param("businessVerifiedYn") String businessVerifiedYn,
@@ -18,12 +23,14 @@ public interface UsersInfoMapper {
                                      @Param("size") Integer size,
                                      @Param("offset") int offset);
 
+    // 총 유저수
     Long getTotalUserCount(@Param("socialYn") String socialYn,    // 명시적 매핑위해 @Param 으로 작성
                            @Param("socialSite") String socialSite,
                            @Param("businessVerifiedYn") String businessVerifiedYn,
                            @Param("influencerYn") String influencerYn,
                            @Param("userStatus") String userStatus);
 
+    // 검색조회
     List<UserInfoDTO> searchUserInfo(@Param("searchType") String searchType,
                                      @Param("searchWord") String searchWord,
                                      @Param("socialYn") String socialYn,
@@ -36,6 +43,7 @@ public interface UsersInfoMapper {
                                      @Param("size") Integer size,
                                      @Param("offset") int offset);
 
+    // 검색된 유저 수
     Long getSearchUserCount(@Param("searchType") String searchType,
                             @Param("searchWord") String searchWord,
                             @Param("socialYn") String socialYn,
@@ -44,4 +52,12 @@ public interface UsersInfoMapper {
                             @Param("influencerYn") String influencerYn,
                             @Param("userStatus") String userStatus,
                             @Param("userAuth") String userAuth);
+
+    // 관리자와 회원자신의 상세정보 조회
+    UserDetailInfoDTO getDetailInfoDto(@Param("userSeq") Long userSeq,
+                                       @Param("userAuth") String userAuth);
+
+    // 다른회원의 상세정보 조회
+    OtherUserInfoDto getOtherUserInfoDto(@Param("userSeq") Long userSeq);
+
 }
