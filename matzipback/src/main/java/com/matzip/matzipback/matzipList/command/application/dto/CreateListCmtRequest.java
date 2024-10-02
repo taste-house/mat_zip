d1e14f2a4ae65c3ef9eeb30534bda28179a217a6
leebackcoding <lcy2825@gmail.com>
2024-10-02 12:38:44 +0900

    refactoring: 리스트 댓글 저장 요청 DTO 리팩토링

--- matzipback/src/main/java/com/matzip/matzipback/matzipList/command/application/dto/CreateListCmtRequest.java
+++ matzipback/src/main/java/com/matzip/matzipback/matzipList/command/application/dto/CreateListCmtRequest.java
@@ -1,15 +1,19 @@
 package com.matzip.matzipback.matzipList.command.application.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Null;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
 @Getter
-@RequiredArgsConstructor
+@Setter
 public class CreateListCmtRequest {
 
-    @NotBlank
-    private final Long listSeq;
-    @NotBlank
-    private final String listCommentContent;
+    @Null(message = "유저 정보를 넣으시면 안됩니다.")
+    private long listCommentUserSeq;
+    @NotNull(message = "리스트 번호에 아무 값도 넣지 않으면 안됩니다.")
+    private Long listSeq; // 리스트 번호
+    @NotBlank(message = "댓글 내용에 아무 내용도 적지 않으면 안됩니다.")
+    private String listCommentContent; // 리스트 댓글 내용
 }
