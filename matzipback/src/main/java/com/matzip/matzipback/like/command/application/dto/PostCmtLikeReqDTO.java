7c83407981a7b44e904bb3827aa877f983e58373
leebackcoding <lcy2825@gmail.com>
2024-10-02 16:37:13 +0900

    refactoring: 게시글 댓글 좋아요 요청 DTO 유효성 추가

--- matzipback/src/main/java/com/matzip/matzipback/like/command/application/dto/PostCmtLikeReqDTO.java
+++ matzipback/src/main/java/com/matzip/matzipback/like/command/application/dto/PostCmtLikeReqDTO.java
@@ -1,5 +1,7 @@
 package com.matzip.matzipback.like.command.application.dto;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Null;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,7 +9,8 @@ import lombok.Setter;
 @Setter
 public class PostCmtLikeReqDTO {
 
+    @Null(message = "좋아요를 누르는 사용자 정보는 필요없습니다.")
     private Long likeUserSeq;
-    private Long postSeq;
+    @NotNull(message = "게시글 댓글 번호를 넣어주세요.")
     private Long postCommentSeq;
 }
