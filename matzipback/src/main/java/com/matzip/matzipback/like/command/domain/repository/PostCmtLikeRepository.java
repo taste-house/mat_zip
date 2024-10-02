ca95e5b53d7e0c8cbac108b874c9f46f1e108ccd
leebackcoding <lcy2825@gmail.com>
2024-10-02 16:36:37 +0900

    refactoring: 게시글 댓글 좋아요 repository 리팩토링

--- matzipback/src/main/java/com/matzip/matzipback/like/command/domain/repository/PostCmtLikeRepository.java
+++ matzipback/src/main/java/com/matzip/matzipback/like/command/domain/repository/PostCmtLikeRepository.java
@@ -2,13 +2,11 @@ package com.matzip.matzipback.like.command.domain.repository;
 
 import com.matzip.matzipback.like.command.domain.aggregate.Like;
 
-import java.util.Optional;
-
 public interface PostCmtLikeRepository  {
 
-    Optional<Like> findByLikeUserSeqAndPostCommentSeq(long likeUserSeq, Long postCommentSeq);
+    boolean existsByLikeUserSeqAndPostCommentSeq(long likeUserSeq, Long postCommentSeq);
 
     Like save(Like newPostCmtLike);
 
-    void deleteById(Long likeSeq);
+    void deleteByLikeUserSeqAndPostCommentSeq(long likeUserSeq, Long postCommentSeq);
 }
