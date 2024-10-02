fb18c74601093a614f9524a9c58e5bfed8244041
leebackcoding <lcy2825@gmail.com>
2024-10-02 11:37:57 +0900

    refactoring: 좋아요한 게시글 조회 mapper 리팩토링

--- matzipback/src/main/java/com/matzip/matzipback/like/query/mapper/LikeQueryMapper.java
+++ matzipback/src/main/java/com/matzip/matzipback/like/query/mapper/LikeQueryMapper.java
@@ -5,12 +5,19 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
-import java.util.Optional;
 
 @Mapper
 public interface LikeQueryMapper {
 
+    // 필요한 메서드인지 생각할 필요가 있음
+    // 1차 수정 미완료 - 창윤
     Long findLikeByUserSeqAndPostCommentSeq(@Param("likeUserSeq") Long userSeq, @Param("postCommentSeq") Long postCommentSeq);
 
-    List<LikedPostDTO> findMyLikedPost(@Param("userSeq") Long userSeq);
+    // 1차 수정 완료 - 창윤
+    List<LikedPostDTO> findMyLikedPost(@Param("userSeq") Long userSeq,
+                                       @Param("offset") long offset,
+                                       @Param("size") int size);
+
+    // 1차 수정 완료 - 창윤
+    long countMyLikePost(@Param("userSeq") Long userSeq);
 }
