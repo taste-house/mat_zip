85e13da4da0ce2c9be2d8989989667d37374a8cc
wildcat222 <wildcat222@naver.com>
2024-10-02 15:33:54 +0900

    chore: Controller 리팩토링

--- matzipback/src/main/java/com/matzip/matzipback/report/command/application/controller/ReportPostController.java
+++ matzipback/src/main/java/com/matzip/matzipback/report/command/application/controller/ReportPostController.java
@@ -7,6 +7,8 @@ import com.matzip.matzipback.report.command.dto.PostCmtReportResMessageDTO;
 import com.matzip.matzipback.report.command.dto.PostReportReqDTO;
 import com.matzip.matzipback.report.command.dto.PostReportResMessageDTO;
 import com.matzip.matzipback.responsemessage.ResponseMessage;
+import com.matzip.matzipback.responsemessage.SuccessCode;
+import com.matzip.matzipback.responsemessage.SuccessResMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,26 +22,8 @@ public class ReportPostController {
     private final ReportPostService reportPostService;
 
     @PostMapping("/post/report")
-    public ResponseEntity<PostReportResMessageDTO> createPostReport(@RequestBody PostReportReqDTO postReportReqDTO) {
-        Report savedPostReport = reportPostService.savePostReport(postReportReqDTO);
-
-        if (savedPostReport != null) { // 신고 성공
-            return ResponseEntity.status(HttpStatus.CREATED).body(new PostReportResMessageDTO(201, ResponseMessage.SAVE_SUCCESS.getMessage(), savedPostReport.getPostSeq()));
-        }
-
-        // 신고 실패 -> 중복 신고는 막음
-        return ResponseEntity.status(HttpStatus.OK).body(new PostReportResMessageDTO(204, ResponseMessage.SAVE_FAIL.getMessage(), -1L));
-    }
-
-    @PostMapping("/postcomment/report")
-    public ResponseEntity<PostCmtReportResMessageDTO> createPostCmtReport(@RequestBody PostCmtReportReqDTO postCmtReportDTO) {
-        Report savedPostCmtReport = reportPostService.savePostCmtReport(postCmtReportDTO);
-
-        if (savedPostCmtReport != null) { // 신고 성공
-            return ResponseEntity.status(HttpStatus.CREATED).body(new PostCmtReportResMessageDTO(201, ResponseMessage.SAVE_SUCCESS.getMessage(), savedPostCmtReport.getPostSeq()));
-        }
-
-        // 신고 실패 -> 중복 신고는 막음
-        return ResponseEntity.status(HttpStatus.OK).body(new PostCmtReportResMessageDTO(204, ResponseMessage.SAVE_FAIL.getMessage(), -1L));
+    public ResponseEntity<SuccessResMessage> createPostReport(@RequestBody PostReportReqDTO postReportReqDTO) {
+        reportPostService.savePostReport(postReportReqDTO);
+        return ResponseEntity.ok(new SuccessResMessage(SuccessCode.BASIC_SAVE_SUCCESS));
     }
 }
