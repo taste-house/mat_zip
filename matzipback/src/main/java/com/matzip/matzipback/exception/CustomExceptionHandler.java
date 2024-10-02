b8f9e45c9c37f65a91f204a5bad6d10c2ecbf5da
leebackcoding <lcy2825@gmail.com>
2024-10-02 15:46:52 +0900

    fix: 글로벌 예외 핸들러 충돌로 인해 하나로 통합 리팩토링

--- matzipback/src/main/java/com/matzip/matzipback/exception/CustomExceptionHandler.java
+++ matzipback/src/main/java/com/matzip/matzipback/exception/CustomExceptionHandler.java
@@ -1,22 +1,57 @@
 package com.matzip.matzipback.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
-public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
+public class CustomExceptionHandler {
 
     @ExceptionHandler(RestApiException.class)
     protected ResponseEntity<ErrorResponse> handleExceptionInternal(RestApiException e) {
+
         ErrorCode errorCode = e.getErrorCode();
         return handleExceptionInternal(errorCode);
     }
 
     private ResponseEntity<ErrorResponse> handleExceptionInternal(ErrorCode errorCode) {
+
         return ResponseEntity
                 .status(errorCode.getHttpStatus())
                 .body(new ErrorResponse(errorCode));
     }
+
+    // MethodArgumentNotValidException 발생 시 메시지 반환
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ValidErrorResponse> handleValidationExceptions(MethodArgumentNotValidException ex) {
+
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+                errors.put(error.getField(), error.getDefaultMessage())
+        );
+
+        return ResponseEntity.badRequest()
+                .body(new ValidErrorResponse(
+                        HttpStatus.BAD_REQUEST.value(), HttpStatus.BAD_REQUEST.name(), errors));
+    }
+
+    // IllegalArgumentException 발생 시 사용자에게 메시지와 400 상태 코드를 반환
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException e) {
+        // 예외 메시지를 클라이언트에 반환
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+
+    @ExceptionHandler(SQLException.class)
+    public ResponseEntity<String> handleSQLException(SQLException e) {
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+    }
+
 }
