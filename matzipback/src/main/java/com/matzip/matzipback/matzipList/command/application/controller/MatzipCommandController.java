eb71cd159bc5d1d66532872662278066b4194b8e
gim-yunhu <rlarjswn0303@gmail.com>
2024-10-03 12:49:45 +0900

    chore: 리스트 맛집 불필요한 코드 제거, 스웨거 어노테이션 추가

--- matzipback/src/main/java/com/matzip/matzipback/matzipList/command/application/controller/MatzipCommandController.java
+++ matzipback/src/main/java/com/matzip/matzipback/matzipList/command/application/controller/MatzipCommandController.java
@@ -5,6 +5,10 @@ import com.matzip.matzipback.matzipList.command.application.dto.CreateMatzipRequ
 import com.matzip.matzipback.matzipList.command.application.dto.DeleteMatzipRequest;
 import com.matzip.matzipback.matzipList.command.application.dto.UpdateMatzipRequest;
 import com.matzip.matzipback.matzipList.command.application.service.MatzipCommandService;
+import com.matzip.matzipback.responsemessage.SuccessCode;
+import com.matzip.matzipback.responsemessage.SuccessResMessage;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -15,35 +19,39 @@ import java.net.URI;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/")
+@Tag(name = "ListMatzip", description = "리스트 맛집")
 public class MatzipCommandController {
 
     private final MatzipCommandService matzipCommandService;
 
     // 맛집 등록
     @PostMapping("/list/matzip/")
-    public ResponseEntity<Void> createMatzip(@Valid @RequestBody CreateMatzipRequest matzipRequest) {
+    @Operation(summary = "리스트 맛집 등록", description = "내 리스트에 맛집을 등록한다.")
+    public ResponseEntity<SuccessResMessage> createMatzip(@Valid @RequestBody CreateMatzipRequest matzipRequest) {
 
-        Long listMatzipSeq = matzipCommandService.createMatzip(matzipRequest);
+        matzipCommandService.createMatzip(matzipRequest);
 
-        return ResponseEntity.created(URI.create("/api/v1/list/matzip" + listMatzipSeq)).build();
+        return ResponseEntity.ok(new SuccessResMessage(SuccessCode.BASIC_SAVE_SUCCESS));
     }
 
     // 맛집 수정
     @PutMapping("/list/matzip/")
-    public ResponseEntity<Void> updateMatzip(@Valid @RequestBody UpdateMatzipRequest updateMatzipRequest) {
+    @Operation(summary = "리스트 맛집 수정", description = "내 리스트에 등록된 맛집을 수정한다.")
+    public ResponseEntity<SuccessResMessage> updateMatzip(@Valid @RequestBody UpdateMatzipRequest updateMatzipRequest) {
 
-        Long listSeq = matzipCommandService.updateMatzip(updateMatzipRequest);
+        matzipCommandService.updateMatzip(updateMatzipRequest);
 
-        return ResponseEntity.created(URI.create("/api/v1/list/matzip" + listSeq)).build();
+        return ResponseEntity.ok(new SuccessResMessage(SuccessCode.BASIC_UPDATE_SUCCESS));
     }
 
     // 맛집 삭제
     @DeleteMapping("/list/matzip/")
-    public ResponseEntity<Void> deleteMatzip(@Valid @RequestBody DeleteMatzipRequest deleteMatzipRequest){
+    @Operation(summary = "리스트 맛집 삭제", description = "내 리스트에 맛집을 삭제한다.")
+    public ResponseEntity<SuccessResMessage> deleteMatzip(@Valid @RequestBody DeleteMatzipRequest deleteMatzipRequest){
 
         matzipCommandService.deleteMatzip(deleteMatzipRequest);
 
-        return ResponseEntity.noContent().build();
+        return ResponseEntity.ok(new SuccessResMessage(SuccessCode.BASIC_DELETE_SUCCESS));
     }
 
 }
