a79fcf56f344002d5bbd825f30343567bb613faa
gim-yunhu <rlarjswn0303@gmail.com>
2024-10-03 12:55:47 +0900

    chore: 리스트 맛집 생성 DTO 어노테이션 수정

--- matzipback/src/main/java/com/matzip/matzipback/matzipList/command/application/dto/CreateMatzipRequest.java
+++ matzipback/src/main/java/com/matzip/matzipback/matzipList/command/application/dto/CreateMatzipRequest.java
@@ -1,15 +1,16 @@
 package com.matzip.matzipback.matzipList.command.application.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
 public class CreateMatzipRequest {
-    @NotBlank
+    @NotNull(message = "리스트 고유번호가 Null이면 안됩니다.")
     private final Long listSeq;
-    @NotBlank
+    @NotNull(message = "음식점 고유번호가 Null이면 안됩니다.")
     private final Long restaurantSeq;
     private final String listMatzipComment;
 }
