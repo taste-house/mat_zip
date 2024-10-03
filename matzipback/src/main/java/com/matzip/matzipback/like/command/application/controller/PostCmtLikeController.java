3a305b12cd39d4b4ff285cb7fd572679b033f7f3
wildcat222 <wildcat222@naver.com>
2024-10-04 01:43:11 +0900

    chore: 게시글 댓글 기능 스웨거 테스트를 위한 설정

--- matzipback/src/main/java/com/matzip/matzipback/like/command/application/controller/PostCmtLikeController.java
+++ matzipback/src/main/java/com/matzip/matzipback/like/command/application/controller/PostCmtLikeController.java
@@ -4,6 +4,8 @@ import com.matzip.matzipback.like.command.application.dto.PostCmtLikeReqDTO;
 import com.matzip.matzipback.like.command.application.service.PostCmtLikeService;
 import com.matzip.matzipback.responsemessage.SuccessCode;
 import com.matzip.matzipback.responsemessage.SuccessResMessage;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,12 +16,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/post")
+@Tag(name = "PostCommentLike", description = "게시글 댓글 좋아요")
 public class PostCmtLikeController {
 
     private final PostCmtLikeService postCmtLikeService;
 
     // 게시글 댓글 좋아요
     @PostMapping("/{postSeq}/postcomment/{postCommentSeq}/like")
+    @Operation(summary = "게시글 댓글 좋아요/좋아요 취소", description = "게시글에 댓글에 좋아요를 표시/취소한다.")
     public ResponseEntity<SuccessResMessage> savePostCmtLike(
             @PathVariable Long postSeq,
             @PathVariable Long postCommentSeq) {
