9bfe5c13a41671e0f43a8ecdd1ace19e28efb629
leebackcoding <lcy2825@gmail.com>
2024-10-04 03:45:12 +0900

    feat: 게시판 즐겨찾기 등록, 취소 infra repository

--- matzipback/src/main/java/com/matzip/matzipback/board/command/infrastructure/repository/BoardInfraRepository.java
+++ matzipback/src/main/java/com/matzip/matzipback/board/command/infrastructure/repository/BoardInfraRepository.java
@@ -1,4 +1,8 @@
 package com.matzip.matzipback.board.command.infrastructure.repository;
 
-public interface BoardInfraRepository {
+import com.matzip.matzipback.board.command.domain.aggregate.FavoriteBoard;
+import com.matzip.matzipback.board.command.domain.repository.BoardDomainRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BoardInfraRepository extends JpaRepository<FavoriteBoard, Long>, BoardDomainRepository {
 }
