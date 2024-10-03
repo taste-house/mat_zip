6df628806e5bb0a1cfd1ca12f44515299124815b
gim-yunhu <rlarjswn0303@gmail.com>
2024-10-03 12:41:23 +0900

    chore: 리스트 맛집 컨트롤러 스웨거 어노테이션 추가

--- matzipback/src/main/java/com/matzip/matzipback/matzipList/command/application/controller/ListCommandController.java
+++ matzipback/src/main/java/com/matzip/matzipback/matzipList/command/application/controller/ListCommandController.java
@@ -7,6 +7,8 @@ import com.matzip.matzipback.matzipList.command.application.dto.UpdateListReques
 import com.matzip.matzipback.matzipList.command.application.service.ListCommandService;
 import com.matzip.matzipback.responsemessage.SuccessCode;
 import com.matzip.matzipback.responsemessage.SuccessResMessage;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -15,12 +17,15 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1")
+@Tag(name = "List", description = "리스트")
 public class ListCommandController {
 
     private final ListCommandService listCommandService;
 
     // 리스트 등록
+    // whoo 1차 수정
     @PostMapping("/list")
+    @Operation(summary = "리스트 등록", description = "리스트를 등록한다.")
     public ResponseEntity<SuccessResMessage> createList(@Valid @RequestBody CreateListRequest listRequest){
 
         Long listSeq = listCommandService.createList(listRequest);
@@ -30,6 +35,7 @@ public class ListCommandController {
 
     // 리스트 삭제
     @DeleteMapping("/list")
+    @Operation(summary = "리스트 삭제", description = "리스트를 삭제한다.")
     public ResponseEntity<SuccessResMessage> deleteList(@Valid @RequestBody DeleteListRequest deleteListRequest){
 
         listCommandService.deleteList(deleteListRequest);
@@ -39,6 +45,7 @@ public class ListCommandController {
 
     // 리스트 수정
     @PutMapping("/list")
+    @Operation(summary = "리스트 수정", description = "리스트를 수정한다.")
     public ResponseEntity<SuccessResMessage> updateList(@Valid @RequestBody UpdateListRequest updateListRequest){
 
         Long listSeq = listCommandService.updateList(updateListRequest);
