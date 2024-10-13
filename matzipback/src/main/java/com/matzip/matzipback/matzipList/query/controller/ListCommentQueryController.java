f92b98f3755ff4e89c8320a09c3b53f660e70633
gim-yunhu <rlarjswn0303@gmail.com>
2024-10-13 12:11:35 +0900

    feat: #331 리스트 댓글 조회 경로 수정

--- matzipback/src/main/java/com/matzip/matzipback/matzipList/query/controller/ListCommentQueryController.java
+++ matzipback/src/main/java/com/matzip/matzipback/matzipList/query/controller/ListCommentQueryController.java
@@ -22,7 +22,7 @@ public class ListCommentQueryController {
     private final ListCommentQueryService listCommentQueryService;
 
     // 리스트에 달린 댓글 조회(삭제되지 않은 댓글만 조회)
-    @GetMapping("/listbox/list/listCmt/{listSeq}")
+    @GetMapping("/listbox/list/{listSeq}/listCmt")
     @Operation(summary = "리스트 댓글 조회", description = "공개된 리스트 댓글을 조회한다.")
     public ResponseEntity<List<ListCommentDTO>> getListComments(@PathVariable("listSeq") Long listSeq){
         return ResponseEntity.ok().body(listCommentQueryService.getListComments(listSeq));
