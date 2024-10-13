955dba34e340a876ff198c075d88c7038e6dca3f
JIYOUNG-22 <dyong0922@naver.com>
2024-10-13 22:53:02 +0900

    chore: vi -> v1 수정

--- matzipback/src/main/java/com/matzip/matzipback/sercurity/util/UserFeignClient.java
+++ matzipback/src/main/java/com/matzip/matzipback/sercurity/util/UserFeignClient.java
@@ -10,9 +10,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 @FeignClient(name = "matzip-user-token-service", url = "localhost:8000", configuration = FeignClientConfig.class)
 public interface UserFeignClient {
 
-    @GetMapping("/user/api/vi/users/email")
+    @GetMapping("/user/api/v1/users/email")
     SuccessSearchResMessage<UserTokenDTO> getUserByEmail(@RequestParam("email") String email);
 
-    @GetMapping("/user/api/vi/users/userseq")
+    @GetMapping("/user/api/v1/users/userseq")
     SuccessSearchResMessage<UserTokenDTO> getUserByUserSeq(@RequestParam("userSeq") Long userSeq);
 }
