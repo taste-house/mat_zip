1a4e7086da2e3108e02e865e517f90660a214932
leebackcoding <lcy2825@gmail.com>
2024-10-14 10:40:04 +0900

    feat: 쪽지방 상세 조회 controller 구현

--- matzipback/src/main/java/com/matzip/matzipback/message/query/controller/MessageQueryController.java
+++ matzipback/src/main/java/com/matzip/matzipback/message/query/controller/MessageQueryController.java
@@ -1,5 +1,6 @@
 package com.matzip.matzipback.message.query.controller;
 
+import com.matzip.matzipback.message.query.dto.MessageDetailDTO;
 import com.matzip.matzipback.message.query.dto.MessageShortDTO;
 import com.matzip.matzipback.message.query.service.MessageQueryService;
 import com.matzip.matzipback.responsemessage.SuccessCode;
@@ -8,10 +9,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -31,5 +29,16 @@ public class MessageQueryController {
         List<MessageShortDTO> messageRoomList = messageQueryService.getMessageList(userSeq);
 
         return ResponseEntity.ok(new SuccessSearchResMessage<>(SuccessCode.BASIC_GET_SUCCESS, messageRoomList));
+    }
+
+    // 쪽지방 상세 조회
+    @GetMapping("/messageList")
+    @Operation(summary = "쪽지방 상세 조회", description = "쪽지방을 상세 조회한다.")
+    public ResponseEntity<SuccessSearchResMessage<?>> getMessageList(
+            @RequestParam("userseq") Long userSeq,
+            @RequestParam("partner") Long partner) {
+
+        List<MessageDetailDTO> messageList = messageQueryService.getMessageRoomDetail(userSeq, partner);
+        return ResponseEntity.ok(new SuccessSearchResMessage<>(SuccessCode.BASIC_GET_SUCCESS, messageList));
     }
 }
