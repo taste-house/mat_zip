e9024aa11a5206b972e5b2d3737d0bceb8973a04
leebackcoding <lcy2825@gmail.com>
2024-10-16 17:10:22 +0900

    feat: 회원 활동 등급 수정, 삭제 Domain service 추가

--- matzipuser/src/main/java/com/matzip/matzipuser/users/command/domain/service/ActiveLevelDomainService.java
+++ matzipuser/src/main/java/com/matzip/matzipuser/users/command/domain/service/ActiveLevelDomainService.java
@@ -1,8 +1,10 @@
 package com.matzip.matzipuser.users.command.domain.service;
 
+import com.matzip.matzipuser.exception.ErrorCode;
+import com.matzip.matzipuser.exception.RestApiException;
+import com.matzip.matzipuser.users.command.application.dto.UpdateActiveLevelDTO;
 import com.matzip.matzipuser.users.command.domain.aggregate.ActiveLevel;
 import com.matzip.matzipuser.users.command.domain.repository.UserActiveLevelRepository;
-import com.matzip.matzipuser.users.command.application.dto.ActiveLevelResDTO;
 import com.matzip.matzipuser.users.command.application.dto.CreateActiveLevelRequestDTO;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
@@ -23,11 +25,27 @@ public class ActiveLevelDomainService {
         return userActiveLevelRepository.findAll();
     }
 
-    public ActiveLevelResDTO saveActiveLevel(CreateActiveLevelRequestDTO createActiveLevelRequestDTO) {
+    public boolean saveActiveLevel(CreateActiveLevelRequestDTO createActiveLevelRequestDTO) {
         ActiveLevel newActiveLevel = modelMapper.map(createActiveLevelRequestDTO, ActiveLevel.class);
 
         ActiveLevel savedActiveLevel = userActiveLevelRepository.save(newActiveLevel);
 
-        return modelMapper.map(savedActiveLevel, ActiveLevelResDTO.class);
+        return savedActiveLevel != null;
+    }
+
+    // 활동 등급 수정
+    public void updateActiveLevel(UpdateActiveLevelDTO updateActiveLevelDTO) {
+
+        ActiveLevel foundActiveLevel = userActiveLevelRepository
+                .findById(updateActiveLevelDTO.getActiveLevelSeq())
+                .orElseThrow(() -> new RestApiException(ErrorCode.NOT_FOUND));
+
+        modelMapper.map(updateActiveLevelDTO, foundActiveLevel);
+    }
+
+    // 활동 등급 삭제
+    public void deleteActiveLevel(Long activeLevelSeq) {
+
+        userActiveLevelRepository.deleteById(activeLevelSeq);
     }
 }
