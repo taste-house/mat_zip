10fa6975c39cb82e464dd70916dba83f355d2a05
leebackcoding <lcy2825@gmail.com>
2024-10-19 17:22:10 +0900

    chore: ReportRepository의 클래스명 변경되어 수정됨

--- matzipback/src/main/java/com/matzip/matzipback/report/command/infrastructure/repository/ReportInfraRepository.java
+++ matzipback/src/main/java/com/matzip/matzipback/report/command/infrastructure/repository/ReportInfraRepository.java
@@ -1,8 +1,8 @@
 package com.matzip.matzipback.report.command.infrastructure.repository;
 
 import com.matzip.matzipback.report.command.domain.aggregate.Report;
-import com.matzip.matzipback.report.command.domain.repository.ReportDomainRepository;
+import com.matzip.matzipback.report.command.domain.repository.ReportRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ReportInfraRepository extends JpaRepository<Report, Long>, ReportDomainRepository {
+public interface ReportInfraRepository extends JpaRepository<Report, Long>, ReportRepository {
 }
